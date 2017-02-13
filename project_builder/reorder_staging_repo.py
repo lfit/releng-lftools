d5429177489227b3e177cace64692828ebdd816b
Andrew Grimberg <agrimberg@linuxfoundation.org>
2017-02-13 06:17:03 -0800

    Nexus2 and Gerrit project builder
    Initial checkin of project build system for Nexus 2 and Gerrit
    
    The script currently does the following:
    
    * takes 2 yaml files (see examples) for configuration
      - settings.yaml == administrative / global settings
      - config.yaml == repository structure to build including nexus
        passwords
    * Walks config.yaml for all repositories and creates targets,
      privileges, roles and users based upon our standard configuration
    
    Presently does not create the Gerrit repositories
    
    * Staging repo re-ordering hack script
    
    NOTE: This does not work against Nexus 3 as the REST API has been
    removed in Nexus 3. Presently scripting Nexus 3 requires groovy
    
    Change-Id: Ia06444a85e167a1e5685f9e569322c9b0e0b8c97
    Signed-off-by: Andrew Grimberg <agrimberg@linuxfoundation.org>

---
+++ project_builder/reorder_staging_repo.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# -*- code: utf-8 -*-
+# vim: sw=4 ts=4 sts=4 et :
+
+#
+# NOTE: This is a hack for forcing the 'Staging Repositories' repo group
+# to be in the correct reverse sorted order. There is a problem with
+# Nexus where it is not doing this like it should be
+#
+
+import argparse
+import sys
+import nexus
+import yaml
+from operator import itemgetter
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-s', '--settings', type=str,
+    help='security and settings yaml file')
+args = parser.parse_args()
+
+if not args.settings:
+    sys.exit('Settings file is required')
+
+
+# open our settings file
+f = open(args.settings, 'r')
+settings = yaml.load(f)
+f.close()
+
+error = False
+if not 'nexus' in settings:
+    print 'nexus needs to be defined'
+    error = True
+
+if not 'user' in settings:
+    print 'user needs to be defined'
+    error = True
+
+if not 'password' in settings:
+    print 'password needs to be defined'
+    error = True
+
+if error:
+    sys.exit(1)
+
+n = nexus.Nexus(settings['nexus'], settings['user'], settings['password'])
+
+try:
+    repo_id = n.get_repo_group('Staging Repositories')
+except LookupError as e:
+    sys.exit("Staging repository 'Staging Repositories' cannot be found")
+
+repo_details = n.get_repo_group_details(repo_id)
+
+sorted_repos = sorted(repo_details['repositories'], key=lambda k: k['id'], reverse=True)
+
+for repos in sorted_repos:
+    del repos['resourceURI']
+    del repos['name']
+
+repo_update = repo_details
+repo_update['repositories'] = sorted_repos
+del repo_update['contentResourceURI']
+del repo_update['repoType']
+
+n.update_repo_group_details(repo_id, repo_update)
---
+++ project_builder/repo_build.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+# -*- code: utf-8 -*-
+# vim: sw=4 ts=4 sts=4 et :
+
+import argparse
+import sys
+import nexus
+import yaml
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-s', '--settings', type=str,
+    help='security and settings yaml file')
+parser.add_argument('-c', '--config', type=str,
+    help='configuration to be created')
+args = parser.parse_args()
+
+if not args.settings:
+    sys.exit('Settings file is required')
+
+if not args.config:
+    sys.exit('Config file is required')
+
+
+# open our settings file
+f = open(args.settings, 'r')
+settings = yaml.load(f)
+f.close()
+
+error = False
+if not 'nexus' in settings:
+    print 'nexus needs to be defined'
+    error = True
+
+if not 'user' in settings:
+    print 'user needs to be defined'
+    error = True
+
+if not 'password' in settings:
+    print 'password needs to be defined'
+    error = True
+
+if not 'email_domain' in settings:
+    print 'email_domain needs to be defined'
+    error = True
+
+if error:
+    sys.exit(1)
+
+# open our config file
+f = open(args.config, 'r')
+config = yaml.load(f)
+f.close()
+
+n = nexus.Nexus(settings['nexus'], settings['user'], settings['password'])
+
+def create_nexus_perms(name, targets, email, password, extra_privs=[]):
+    # Create target
+    try:
+        target_id = n.get_target(name)
+    except LookupError as e:
+        target_id = n.create_target(name, targets)
+
+    # Create privileges
+    privs_set = [
+            'create',
+            'delete',
+            'read',
+            'update',
+        ]
+
+    privs = {}
+    for priv in privs_set:
+        try:
+            privs[priv] = n.get_priv(name, priv)
+        except LookupError as e:
+            privs[priv] = n.create_priv(name, target_id, priv)
+
+    # Create Role
+    try:
+        role_id = n.get_role(name)
+    except LookupError as e:
+        role_id = n.create_role(name, privs)
+
+    # Create user
+    try:
+        n.get_user(name)
+    except LookupError as e:
+        n.create_user(name, email, role_id, password, extra_privs)
+
+def do_build_repo(repo, repoId, config, base_groupId):
+    print 'Building for %s.%s' % (base_groupId, repo)
+    groupId = '%s.%s' % (base_groupId, repo)
+    target = '^/%s/.*' % groupId.replace('.', '[/\.]')
+    if 'extra_privs' in config:
+        extra_privs = config['extra_privs']
+    else:
+        extra_privs = []
+    # print '%s\t\t\t%s' % (repoId, config['password'])
+    create_nexus_perms(repoId, [target], settings['email_domain'],
+        config['password'], extra_privs)
+    if 'repositories' in config:
+        for sub_repo in config['repositories']:
+            sub_repo_id = '%s-%s' % (repoId, sub_repo)
+            do_build_repo(sub_repo, sub_repo_id, config['repositories'][sub_repo],
+                groupId)
+
+for repo in config['repositories']:
+    do_build_repo(repo, repo, config['repositories'][repo], config['base_groupId'])
