e74a07663650c46876d50c3306cf32c769d0ce15
Thanh Ha <thanh.ha@linuxfoundation.org>
2017-03-04 12:04:58 -0500

    Add basic setup.py
    Initialize lftools as a project on pypi.
    
    Change-Id: I901ecd512a856f2693da773ae08861ca1bfbf7c8
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

---
+++ setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+
+setup(
+    name='lftools',
+    version='0.0.2',
+    author='Thanh Ha',
+    author_email='thanh.ha@linuxfoundation.org',
+    url='',
+    description='',
+    long_description=(
+        'The main purpose of Spectrometer is to deliver transparent '
+        'statistics of contributions to OpenDaylight Project. It collects '
+        'activity data such as 1. commits and number of code lines changed '
+        'from ODL Git repositories, 2. reviews from Gerrit, or 3. activities '
+        'related to each project from mailing lists and presents the '
+        'statistics in a user-friendly manner.'),
+    license='EPL',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+    ],
+    scripts=[
+        'shell/patch-odl-release',
+        'shell/version',
+    ],
+)
