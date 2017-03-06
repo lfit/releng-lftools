011aae5502790adf441c909afa344250e67811be
Thanh Ha <thanh.ha@linuxfoundation.org>
2017-03-05 19:30:23 -0500

    Make create version tests easier
    This patch allows version tests to simply be placing files into a
    directory and comparing the test results from an input pom and an
    expected pom.
    
    Also add a test to validate bump order.
    
    Change-Id: Ide4da6f721223bc4d3edab79db9008e4cae730c6
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- tests/test_version.py
+++ tests/test_version.py
@@ -16,29 +16,26 @@ FIXTURE_DIR = os.path.join(
 
 
 @pytest.mark.datafiles(
-    os.path.join(FIXTURE_DIR, 'pom.xml'),
-    os.path.join(FIXTURE_DIR, 'pom.xml.expected-bump'),
+    os.path.join(FIXTURE_DIR, 'version_bump'),
     )
 def test_version_bump(cli_runner, datafiles):
     os.chdir(datafiles)
-
-    # Version bump should bump versions by x.(y+1).z
     result = cli_runner.invoke(cli.cli, ['version', 'bump', 'TestRelease'])
-    assert filecmp.cmp('pom.xml', 'pom.xml.expected-bump')
+
+    for _file in datafiles.listdir():
+        pom = str(_file) + '/pom.xml'
+        expected_pom = str(_file) + '/pom.xml.expected'
+        assert filecmp.cmp(pom, expected_pom)
 
 
 @pytest.mark.datafiles(
-    os.path.join(FIXTURE_DIR, 'pom.xml'),
-    os.path.join(FIXTURE_DIR, 'pom.xml.expected-release'),
-    os.path.join(FIXTURE_DIR, 'pom.xml.expected-release-bump'),
+    os.path.join(FIXTURE_DIR, 'version_release'),
     )
 def test_version_release(cli_runner, datafiles):
     os.chdir(datafiles)
-
-    # Version release should modify SNAPSHOT to TestRelease
     result = cli_runner.invoke(cli.cli, ['version', 'release', 'TestRelease'])
-    assert filecmp.cmp('pom.xml', 'pom.xml.expected-release')
 
-    # Post release bump should bump versions by x.y.(z+1) and revert back to SNAPSHOT
-    result = cli_runner.invoke(cli.cli, ['version', 'bump', 'TestRelease'])
-    assert filecmp.cmp('pom.xml', 'pom.xml.expected-release-bump')
+    for _file in datafiles.listdir():
+        pom = str(_file) + '/pom.xml'
+        expected_pom = str(_file) + '/pom.xml.expected'
+        assert filecmp.cmp(pom, expected_pom)
