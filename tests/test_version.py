b21001a89a333876c3879497a20172218ce53149
Thanh Ha <thanh.ha@linuxfoundation.org>
2017-03-06 15:48:28 -0500

    Run tests against both Python 2 and Python 3
    Change-Id: I00526b5111147e5c2a26b82766dfb3e6e2777508
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- tests/test_version.py
+++ tests/test_version.py
@@ -19,7 +19,7 @@ FIXTURE_DIR = os.path.join(
     os.path.join(FIXTURE_DIR, 'version_bump'),
     )
 def test_version_bump(cli_runner, datafiles):
-    os.chdir(datafiles)
+    os.chdir(str(datafiles))
     result = cli_runner.invoke(cli.cli, ['version', 'bump', 'TestRelease'])
 
     for _file in datafiles.listdir():
@@ -32,7 +32,7 @@ def test_version_bump(cli_runner, datafiles):
     os.path.join(FIXTURE_DIR, 'version_release'),
     )
 def test_version_release(cli_runner, datafiles):
-    os.chdir(datafiles)
+    os.chdir(str(datafiles))
     result = cli_runner.invoke(cli.cli, ['version', 'release', 'TestRelease'])
 
     for _file in datafiles.listdir():
