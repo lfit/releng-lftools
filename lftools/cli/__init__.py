9291391e220eefbb8bac04a079b87f54a6fc9a70
Thanh Ha <thanh.ha@linuxfoundation.org>
2017-03-07 11:53:37 -0500

    Remove Contributor list
    We can get the contributor list to a file by checking git-log which
    would provide more accurate data anyway.
    
    Change-Id: I538c205021e08f1fa3c7faf9d2957c648f362b07
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- lftools/cli/__init__.py
+++ lftools/cli/__init__.py
@@ -6,11 +6,12 @@
 # are made available under the terms of the Eclipse Public License v1.0
 # which accompanies this distribution, and is available at
 # http://www.eclipse.org/legal/epl-v10.html
-#
-# Contributors:
-#   Thanh Ha - Initial implementation
 ##############################################################################
 """CLI main for lftools."""
+
+__author__ = 'Thanh Ha'
+
+
 import click
 from lftools.cli.version import version
 
