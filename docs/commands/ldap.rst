32e17d90755ebcbddeaeea7c8b32f0f598a9d1e9
Anil Belur <abelur@linuxfoundation.org>
2017-04-18 19:22:58 +1000

    LDAP project builder
    This script creates a ldap project group and adds initial committers
    to the group. LDAP server settings are read from secure-settings.yaml
    and project configuration is read from config.yaml.
    
    Usage:
    
    lftools ldap create -c config-ldap.yaml -s secure-settings-ldap.yaml
    
    Change-Id: Id8931283505c32c86da31f9ad11b1370f9f6fe9b
    Signed-off-by: Anil Belur <abelur@linuxfoundation.org>

---
+++ docs/commands/ldap.rst
@@ -0,0 +1,25 @@
+****
+LDAP
+****
+
+The ldap command creates a LDAP project group and adds initial committers
+to the group.
+
+.. program-output:: lftools ldap --help
+
+.. note::
+
+   python-ldap requires openldap development packages installed. For CentOS 7
+   install openldap-dev rpm and for Ubuntu install libldap2-dev, libsasl2-dev
+   packages.
+
+Commands
+========
+
+.. contents:: Nexus Commands
+    :local:
+
+create
+------
+
+.. program-output:: lftools ldap create --help
---
+++ lftools/ldap/cmd.py
@@ -0,0 +1,40 @@
+# -*- code: utf-8 -*-
+# @License EPL-1.0 <http://spdx.org/licenses/EPL-1.0>
+##############################################################################
+# Copyright (c) 2017 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+"""Library for creating LDAP groups."""
+
+__author__ = 'Anil Belur'
+
+
+import click
+from lftools.ldap import groups as ldapcmd
+
+
+@click.group()
+@click.pass_context
+def ldap(ctx):
+    """Provide an interface to ldap."""
+    pass
+
+
+@click.command()
+@click.option(
+    '-c', '--config', type=str, required=True,
+    help='Project config file on project and member heirarchy.')
+@click.option(
+    '-s', '--secure-settings', type=str, required=True,
+    help='Config file containing LDAP administrative settings.')
+@click.pass_context
+def create(ctx, config, secure_settings):
+    """Create LDAP groups."""
+    ldapcmd.create_ldap_groups_ids(config, secure_settings)
+
+
+ldap.add_command(create)
