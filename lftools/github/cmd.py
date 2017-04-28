79d627b62763c3044118ae1be1b152397c9d14b7
Jeremy Phelps <jphelps@linuxfoundation.org>
2017-04-28 10:40:34 -0500

    Add github funcs to lftools
    [RELENG-85] Add a way to create a hook,create branch protections,
    and create a repository.
    
    Change-Id: Ie4f893108c7624dd9162ae66466e4f808e54bcb3
    Signed-off-by: Jeremy Phelps <jphelps@linuxfoundation.org>

---
+++ lftools/github/cmd.py
@@ -0,0 +1,30 @@
+# -*- code: utf-8 -*-
+"""Github tools cmd module"""
+# @License EPL-1.0 <http://spdx.org/licenses/EPL-1.0>
+##############################################################################
+# Copyright (c) 2017 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+
+__author__ = 'Jeremy Phelps'
+
+from github import Github, Repository
+
+def github():
+    return Github()
+
+def protect_branch(branch_name, repo):
+    """Add branch protections"""
+    pass
+
+def create_webhook(name, config, events=[], active=False):
+    gh = github()
+    repo = gh.get_user().get_repo('ci-managment')
+    repo.create_hook(name, config, events=events, active=active)
+
+def get_user(user):
+    pass
