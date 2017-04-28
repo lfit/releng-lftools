9101fc5a0b0da197ca32ab7e36b802ff90a4b3ae
Jeremy Phelps <jphelps@linuxfoundation.org>
2017-04-28 10:40:34 -0500

    Add github funcs to lftools
    Change-Id: Ie4f893108c7624dd9162ae66466e4f808e54bcb3
    Signed-off-by: Jeremy Phelps <jphelps@linuxfoundation.org>

---
+++ tests/test_github.py
@@ -0,0 +1,16 @@
+# -*- code: utf-8 -*-
+# @License EPL-1.0 <http://spdx.org/licenses/EPL-1.0>
+##############################################################################
+# Copyright (c) 2017 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+
+from lftools.github import cmd 
+
+def test_get_repo():
+    repo = cmd.get_repo()
+    assert repo
