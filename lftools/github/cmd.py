838f42043f067f08b4a1b5ef28928fab6fd6cfde
Jeremy Phelps <jphelps@linuxfoundation.org>
2017-04-28 10:40:34 -0500

    Add github funcs to lftools
    [RELENG-85] Add a way to create a hook,create branch protections,
    and create a repository.
    
    Change-Id: Ie4f893108c7624dd9162ae66466e4f808e54bcb3
    Signed-off-by: Jeremy Phelps <jphelps@linuxfoundation.org>

---
+++ lftools/github/cmd.py
@@ -0,0 +1,57 @@
+# -*- code: utf-8 -*-
+"""Github tools cmd module."""
+# @License EPL-1.0 <http://spdx.org/licenses/EPL-1.0>
+##############################################################################
+# Copyright (c) 2017 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+
+__author__ = 'Jeremy Phelps'
+import sys
+import yaml
+
+from github import Github
+
+def github(settings_file):
+    '''Set up an authenticated Github User with the base github
+       api url and a personal access token.'''
+    with open(settings_file, 'r') as settings_f:
+        settings = yaml.safe_load(settings_f)
+
+    for setting in ['base_url', 'token']:
+        if not setting in settings:
+            sys.exit('{} needs to be defined'.format(setting))
+
+    return Github(settings['token'], base_url=settings['base_url'])
+
+def protect_branches(config_file, settings_file, repo_name):
+    """Protect repository branches via the Github api."""
+    gh_object = github(settings_file)
+    repo = gh_object.get_user().get_repo(repo_name)
+
+    with open(config_file, 'r') as config_f:
+        configs = yaml.safe_load(config_f)
+
+    # do stuff with configs here
+
+def create_webhooks(config_file, settings_file, repo_name):
+    """Create a webhook via the Github api."""
+    gh_object = github(settings_file)
+    repo = gh_object.get_user().get_repo(repo_name)
+
+    with open(config_file, 'r') as config_f:
+        configs = yaml.safe_load(config_f)
+
+    for hook in configs:
+        for key in hook:
+            try:
+                repo.create_hook(hook[key]['name'],
+                                 hook[key]['config'],
+                                 events=hook[key]['events'],
+                                 active=hook[key]['active'])
+            except KeyError as error:
+                sys.exit('Key Error: {}'.format(error))
