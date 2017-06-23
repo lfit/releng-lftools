5e846536a4988c4decb2764b7028eb46ac61bf89
Jessica Wagantall <jwagantall@linuxfoundation.org>
2017-06-23 14:23:44 -0700

    Add Jenkins script for configuration files
    This script will help creating credentials and
    configuration files per repo automatically.
    
    Issue-ID: RELENG-2
    Change-Id: I250bb8f2bf283b46947480fc55cc9d49b4b3fd8a
    Signed-off-by: Jessica Wagantall <jwagantall@linuxfoundation.org>
    Signed-off-by: Andrew Grimberg <agrimberg@linuxfoundation.org>

---
+++ lftools/jenkins/templates/modify-server-id.groovy
@@ -0,0 +1,63 @@
+// @License EPL-1.0 <http://spdx.org/licenses/EPL-1.0>
+// #############################################################################
+// Copyright (c) 2017, 2018, 2019 The Linux Foundation and others.
+//
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+//
+// #############################################################################
+
+import org.jenkinsci.lib.configprovider.model.Config
+import org.jenkinsci.plugins.configfiles.maven.MavenSettingsConfig
+import org.jenkinsci.plugins.configfiles.maven.security.ServerCredentialMapping
+import org.jenkinsci.plugins.configfiles.maven.MavenSettingsConfig.MavenSettingsConfigProvider
+import org.jenkinsci.plugins.configfiles.GlobalConfigFiles
+import org.jenkinsci.plugins.configfiles.ConfigFileStore
+
+// Function to delete a server id on a config file
+def removeServerId = { configFileId, serverId ->
+    ConfigFileStore sto = GlobalConfigFiles.get();
+    Config c = sto.getById("${configFileId}-settings");
+    def string_id = c.name;
+    def List<ServerCredentialMapping> map_list = c.serverCredentialMappings;
+    println "These are the ServerIds for ${string_id} settings file: ${configFileId}-settings"
+    //map_list.eachWithIndex { item, index ->
+    //   println item.serverId;
+    //}
+    def indexToRemove;
+    map_list.eachWithIndex { item, index ->
+        if( item.serverId == serverId ) {
+            println "Removing serverId " + item.serverId + " from config " + configFileId;
+            indexToRemove = index;
+        }
+    }
+    map_list.remove(indexToRemove);
+    c.serverCredentialMappings = map_list;
+}
+
+// Function to add a server id on a config file
+def addServerId = { configFileId, serverId ->
+    List<ServerCredentialMapping> mappings
+    ServerCredentialMapping mapping
+    ConfigFileStore sto = GlobalConfigFiles.get();
+    Config c = sto.getById("${configFileId}-settings");
+    def string_id = c.name;
+    def List<ServerCredentialMapping> map_list = c.serverCredentialMappings;
+    println "Adding ${serverId} to ${string_id} settings file: ${configFileId}-setting"
+    mappings = new ArrayList<ServerCredentialMapping>();
+    mapping = new ServerCredentialMapping(serverId, configFileId);
+    mappings.add(mapping);
+    map_list.addAll(mappings);
+    c.serverCredentialMappings = map_list;
+}
+
+// Function to modify a server id on a config file
+def modifyServerId = { configFileId, oldServerId, newServerId ->
+    println "Renaming serverId " + oldServerId + " to " + newServerId + " from config " + configFileId;
+    removeServerId(configFileId, oldServerId)
+    addServerId(configFileId, newServerId)
+}
+
+modifyServerId("new_project", "onap-site", "openecomp-site")
