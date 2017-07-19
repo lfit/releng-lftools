bccda74c4416da9fa91a4452e8bf152701c13ba0
Thanh Ha <thanh.ha@linuxfoundation.org>
2017-07-19 18:50:24 -0400

    Add a license header scanner
    Simple scanner to check code files for a license header. Does not care
    about the exact formatting of the license header as long as all the text
    exists in the correct order.
    
    This version only supports headers using '#' as the comment string.
    
    Issue: RELENG-279
    Change-Id: Id4030f040c3de4350c59776ed21eed497e5d6f8d
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

---
+++ docs/commands/license.rst
@@ -0,0 +1,16 @@
+*******
+License
+*******
+
+.. program-output:: lftools license --help
+
+Commands
+========
+
+.. contents:: License Commands
+    :local:
+
+check
+-----
+
+.. program-output:: lftools license check --help
---
+++ lftools/license.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2017 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+"""Scans code for a valid license header."""
+
+__author__ = 'Thanh Ha'
+
+
+import os
+import re
+import sys
+
+
+def get_header_text(_file):
+    """Scan a file and pulls out the license header.
+
+    Returns a string containing the license header with newlines and copyright
+    lines stripped.
+
+    Note: This function only supports '#' comments for license headers.
+    """
+    text = ''
+    with open(_file, 'r') as data:
+        lines = data.readlines()
+        for line in lines:
+            result = re.search(r'\s*[#]', line)
+            if not result:
+                break
+            string = re.sub(r'^\s*#+', '', line).strip()
+            if bool(re.match('Copyright', string, re.I)):  # Ignore the Copyright line
+                continue
+            text += ' {}'.format(string)
+    # Strip unnecessary spacing
+    text = re.sub('\s+', ' ', text).strip()
+    return text
+
+
+def check_license(license_file, code_file):
+    """Compare a file with the provided license header.
+
+    Reports if license header is missing or does not match the text of
+    license_file.
+    """
+    license_header = get_header_text(license_file)
+    code_header = get_header_text(code_file)
+
+    if not license_header in code_header:
+        print('ERROR: {} is missing or has incorrect license header.'.format(code_file))
+        return 1
+
+    return 0
+
+
+def check_license_directory(license_file, directory, extension="py"):
+    """Searches a directory for files and calls check_license()"""
+
+    missing_license = False
+
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file.endswith(".{}".format(extension)):
+                if check_license(license_file, os.path.join(root, file)):
+                    missing_license = True
+
+    if missing_license:
+        sys.exit(1)
+
+    print('Scan completed did not detect any files missing license headers.')
