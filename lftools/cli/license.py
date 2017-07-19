03aa74eecd510315351bb511140d8157948b98b5
Thanh Ha <thanh.ha@linuxfoundation.org>
2017-07-19 18:50:24 -0400

    Add a license header scanner
    Simple scanner to check code files for a license header. Does not care
    about the exact formatting of the license header as long as all the text
    exists in the correct order.
    
    This version only supports headers using '#' as the comment string.
    
    Issue: RELENG-279
    Change-Id: Id4030f040c3de4350c59776ed21eed497e5d6f8d
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

---
+++ lftools/cli/license.py
@@ -0,0 +1,65 @@
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2017 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+"""Scan code for license headers."""
+
+__author__ = 'Thanh Ha'
+
+
+import sys
+
+import click
+from lftools.license import check_license
+from lftools.license import check_license_directory
+
+
+@click.group()
+@click.pass_context
+def license(ctx):
+    """Scan code for license headers."""
+    pass
+
+
+@click.command()
+@click.argument('source')
+@click.option('-l', '--license', default='license-header.txt',
+              help='License header file to compare against.')
+@click.pass_context
+def check(ctx, license, source):
+    """Check files for missing license headers.
+
+    Does not care if about line formatting of the license as long as all of the
+    text is there and in the correct order.
+
+    Note: This code only supports '#' comments for license headers.
+    """
+    exit_code = check_license(license, source)
+    sys.exit(exit_code)
+
+
+@click.command(name='check-dir')
+@click.argument('directory')
+@click.option('-e', '--extension', default='py',
+              help='File extension to search for.')
+@click.option('-l', '--license', default='license-header.txt',
+              help='License header file to compare against.')
+@click.pass_context
+def check_directory(ctx, license, directory, extension):
+    """Check directory for files missing license headers.
+
+    Does not care if about line formatting of the license as long as all of the
+    text is there and in the correct order.
+
+    Note: This code only supports '#' comments for license headers.
+    """
+    check_license_directory(license, directory, extension)
+
+
+license.add_command(check)
+license.add_command(check_directory)
---
+++ tests/test_license.py
@@ -0,0 +1,58 @@
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2017 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+
+import difflib
+from distutils import dir_util
+import filecmp
+import os
+
+import click
+import pytest
+
+from lftools import cli
+
+
+FIXTURE_DIR = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)),
+    'fixtures',
+    )
+
+
+@pytest.mark.datafiles(
+    os.path.join(FIXTURE_DIR, 'license'),
+    )
+def test_check_license(cli_runner, datafiles):
+    os.chdir(str(datafiles))
+
+    # Check that license checker passes when file has license.
+    result = cli_runner.invoke(cli.cli, ['license', 'check', 'license.py'])
+    assert result.exit_code == 0
+
+    # Check that license checker fails when file is missing license.
+    result = cli_runner.invoke(cli.cli, ['license', 'check', 'no_license1.py'])
+    assert result.exit_code == 1
+
+
+@pytest.mark.datafiles(
+    os.path.join(FIXTURE_DIR, 'license'),
+    )
+def test_check_license_directory(cli_runner, datafiles):
+    os.chdir(str(datafiles))
+
+    # Check that check-dir fails due to directory containing files
+    # with no license.
+    result = cli_runner.invoke(cli.cli, ['license', 'check-dir', '.'])
+    assert result.exit_code == 1
+
+    # Check that check-dir passes when directory contains files with licenses
+    os.remove('no_license1.py')
+    os.remove('no_license2.py')
+    result = cli_runner.invoke(cli.cli, ['license', 'check-dir', '.'])
+    assert result.exit_code == 0
