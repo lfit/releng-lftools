286378a89aeec7a169320f5d492e9840b50300a6
Thanh Ha <thanh.ha@linuxfoundation.org>
2017-07-25 16:47:16 -0400

    Ignore linkchecking Google blog
    Our CI systems appear to be failing to validate against this URL.
    Perhaps Google blocked us?
    
    Change-Id: Ie2047cd7f4a9fe94491c440bde93b67bdc3fc904
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- docs/conf.py
+++ docs/conf.py
@@ -355,3 +355,8 @@ texinfo_documents = [
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'https://docs.python.org/': None}
+
+# Linkcheck Options
+linkcheck_ignore= [
+    r'https://testing\.googleblog\.com/.*',
+]
