a1ed714d150f4880f470a4053fb7b293ac035ba7
Jessica Wagantall <jwagantall@linuxfoundation.org>
2017-07-28 00:04:34 -0700

    Implement lftools logger
    Add generic logger configuration file
    to be used by lftools scripts
    
    JIRA: RELENG-321
    Change-Id: I8fef663dd6b02947f7722424ecce6e2cc78debec
    Signed-off-by: Jessica Wagantall <jwagantall@linuxfoundation.org>

--- lftools/__init__.py
+++ lftools/__init__.py
@@ -14,3 +14,27 @@
 
 __author__ = 'Thanh Ha'
 __version__ = '0.7.0-dev'
+
+import logging
+import logging.config
+import os
+
+def find_log_ini( file_name ):
+  homedir = os.path.expanduser('~')
+  log_file_home = os.path.abspath(os.path.join(homedir, ".config/lftools", file_name))
+  log_file_etc = os.path.abspath(os.path.join("/etc/lftools", file_name))
+  if os.path.exists(log_file_home):
+      file_path = log_file_home
+  elif os.path.exists(log_file_etc):
+      file_path = log_file_etc
+  else:
+      file_path=('etc/%s' % file_name)
+  return file_path
+
+log_ini_file = "logging.ini"
+log_ini_file_path = find_log_ini(log_ini_file)
+
+logging.config.fileConfig(log_ini_file_path)
+log = logging.getLogger(__name__)
+
+log.info("Using logger config file: %s" % log_ini_file_path)
