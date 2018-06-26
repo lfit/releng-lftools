9f070e5d0157fed342f4ffa12ccc5c6fa41a12c8
Aric Gardner <agardner@linuxfoundation.org>
2018-06-26 15:16:08 -0400

    extends lftools adding scripts for ldap lookups
    prereqs:
    For ldap lookups to work you must be on the VPN
    log in to any collab system and grab /etc/ipa/ca.crt
    in /etc/openldap/ldap.conf, add 'TLS_CACERT /path/to/ipa.ca'
    
    ---
    ldap-lookup csv --group:
    Provides a way to ouput: ldap-group "lfid","Full Name","email@address"
    
    Output example:
    
    opnfv-gerrit-releng-submitters,fdegir,Fatih Degirmenci,fdegir@gmail.com
    opnfv-gerrit-releng-submitters,trozet,Tim Rozet,trozet@redhat.com
    
    ---
    
    ldap-lookup yaml4info --group:
    
    calls lftools csv and converts to yaml
    $1 is the ldap-group name you would like to build yaml for your
    INFO.yaml file
    
    Output example:
        - name: 'Aric Gardner'
          email: 'agardner@linuxfoundation.org'
          company: 'linuxfoundation'
          uid: ' agardner'
          timezone: ''
        - name: 'Trevor Bramwell'
          email: 'tbramwell@linuxfoundation.org'
          company: 'linuxfoundation'
          uid: ' bramwelt'
          timezone: ''
    
    ---
    lftools ldap verifyinfofile --gerritclonebase --ldapgroup --repo:
    
    Checks a projects INFO.yaml against what is in LDAP
    and outputs and discrepancies
    
    Output example:
    
    Diff LDAP of releng against INFO.yaml of releng
    These users are listed as commiters in LDAP and not in the INFO.yaml
        - name: 'Jenkins Ericsson'
          email: 'opnfvuser@gmail.com'
          id: 'jenkins-ericsson'
    Exit status = 1
    
    ---
    lftools parseinfofile --file:
    
    Grabs subsets of the info file, in its most basic form it outputs the
    lfids all all users in an info file
    
    Output example:
    
          id: 'bramwelt'
          id: 'fdegir'
          id: 'agardner'
          id: 'trozet'
          id: 'mrichomme'
          id: 'jose.lausuch'
          id: 'r-mibu'
          id: 'm00133142'
          id: 'SerenaFeng'
          id: 'yrobla'
          id: 'mchandras'
          id: 'lukehinds'
    
    Issue: RELENG-407
    Change-Id: I4f4055441042d790008754bb085447f52e1c1a78
    Signed-off-by: Aric Gardner <agardner@linuxfoundation.org>

---
+++ lftools/cli/ldap_cli.py
@@ -0,0 +1,153 @@
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2017 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+"""
+Generate a CVS of OPNFV Commiters.
+
+Prereqs:
+yum install python-devel openldap-devel, and pip install python-ldap
+"""
+
+from __future__ import print_function
+import subprocess
+import sys
+import ldap
+import click
+
+@click.group()
+@click.pass_context
+def ldap_cli(ctx):
+    """LDAP TOOLS."""
+pass
+
+
+@click.command()
+@click.option('--groups', envvar='groups', type=str, required=True,
+              help='LDAP group name.')
+@click.pass_context
+def yaml4info(ctx, groups):
+    """Build yaml of commiters for your INFO.yaml."""
+    status = subprocess.call(['yaml4info', groups])
+    sys.exit(status)
+
+@click.command()
+@click.option('--gerritclonebase', envvar='gerritclonebase', type=str, required=True,
+              help='url for clone base eg: https://gerrit.opnfv.org/gerrit/.')
+@click.option('--ldapgroup', envvar='ldapgroup', type=str, required=True,
+              help='LDAP group name.')
+@click.option('--repo', envvar='repo', type=str, required=True,
+              help='repo name eg: releng.')
+@click.pass_context
+def verifyinfofile(ctx, gerritclonebase, ldapgroup, repo):
+    """Verify INFO.yaml against LDAP group."""
+    status = subprocess.call(['verifyinfofile', gerritclonebase, ldapgroup, repo])
+    sys.exit(status)
+
+
+@click.command()
+@click.option('--ldapserver', default='ldaps://pdx-wl-lb-lfldap.web.codeaurora.org',
+              envvar='LDAP_SERVER', type=str, required=True)
+@click.option('--ldapuserbase', default='ou=Users,dc=freestandards,dc=org',
+              envvar='LDAP_USER_BASE_DN', type=str, required=True)
+@click.option('--ldapgroupbase', default='ou=Groups,dc=freestandards,dc=org',
+              envvar='LDAP_GROUP_BASE_DN', type=str, required=True)
+@click.option('--groups', envvar='groups', type=str, required=True)
+@click.pass_context
+def csv(ctx, ldapserver, ldapgroupbase, ldapuserbase, groups):
+    """Query an Ldap server."""
+    # groups needs to be a list
+    groups = groups.split(' ')
+
+    def ldap_connect(ldap_object):
+        """Start the connection to LDAP."""
+        try:
+            ldap_object.protocol_version = ldap.VERSION3
+            ldap_object.simple_bind_s()
+        except ldap.LDAPError as e:
+            if type(e.message) == dict and e.message.has_key('desc'):
+                print(e.message['desc'])
+            else:
+                print(e)
+            sys.exit(0)
+
+    def eprint(*args, **kwargs):
+        """Print to stderr."""
+        print(*args, file=sys.stderr, **kwargs)
+    def ldap_disconnect(ldap_object):
+        """Stop the connnection to LDAP."""
+        ldap_object.unbind_s()
+
+    def ldap_query(ldap_object, dn, search_filter, attrs):
+        """Perform an LDAP query and return the results."""
+        try:
+            ldap_result_id = ldap_object.search(dn, ldap.SCOPE_SUBTREE, search_filter, attrs)
+            result_set = []
+            while 1:
+                result_type, result_data = ldap_object.result(ldap_result_id, 0)
+                if (result_data == []):
+                    break
+                else:
+                    # if you are expecting multiple results you can append them
+                    # otherwise you can just wait until the initial result and break out
+                    if result_type == ldap.RES_SEARCH_ENTRY:
+                        result_set.append(result_data)
+            return result_set
+        except ldap.LDAPError as e:
+            sys.exit(1)
+
+    def package_groups(groups):
+        """
+        Package a set of groups from LDIF into a Python dictionary.
+
+        containing the groups member uids.
+        """
+        group_list = []
+        cut_length = len(ldapuserbase)+1
+        for group in groups:
+            group_d = dict(name=group[0][0])
+            members = []
+            for group_attrs in group:
+                for member in group_attrs[1]['member']:
+                    members.append(member[:-cut_length])
+            group_d['members'] = members
+            group_list.append(group_d)
+        return group_list
+
+    def user_to_csv(user):
+        """Covert LDIF user info to CSV of uid,mail,cn."""
+        attrs = user[0][0][1]
+        return ",".join([attrs['uid'][0], attrs['cn'][0], attrs['mail'][0]])
+
+    def main(groups):
+        """Preform an LDAP query."""
+        l = ldap.initialize(ldapserver)
+        ldap_connect(l)
+        for arg in groups:
+            groups = ldap_query(l, ldapgroupbase, "cn=%s" % arg, ["member"])
+            group_dict = package_groups(groups)
+            cut_length = len(ldapgroupbase)+1
+            for group_bar in group_dict:
+                group_name = group_bar['name'][3:-cut_length]
+                for user in group_bar['members']:
+                    user_info = ldap_query(l, ldapuserbase, user, ["uid", "cn", "mail"])
+                    try:
+                        print("%s,%s" % (group_name, user_to_csv(user_info)))
+                    except:
+                        eprint("Error parsing user: %s" % user)
+                        continue
+        ldap_disconnect(l)
+    main(groups)
+
+
+##################################
+
+
+ldap_cli.add_command(csv)
+ldap_cli.add_command(yaml4info)
+ldap_cli.add_command(verifyinfofile)
---
+++ lftools/cli/parseinfofile.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python2
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2017 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+"""Script to extract committer information from an INFO.yaml file."""
+
+import yaml
+import click
+
+@click.command()
+@click.option('--file', envvar='', type=str, required=True,
+              help='File to extract from')
+@click.option('--full', envvar='', type=str, required=False,
+              help='Output name email and id ')
+@click.option('--single', envvar='', type=str, required=False,
+              help='Output for a specific lifd')
+
+@click.pass_context
+def parseinfofile(ctx, file, full, single):
+    with open(file, 'r') as yaml_file:
+        project = yaml.safe_load(yaml_file)
+
+    def list_committers(full, single, project):
+        """List commiters from the INFO.yaml file."""
+        lookup = project.get('committers', [])
+        if 'committers' not in project:
+            exit(0)
+
+        for item in lookup:
+            if full:
+                print("    - name: '%s'" % item['name'])
+                print("      email: '%s'" % item['email'])
+                print("      id: '%s'" % item['id'])
+            elif single and item['id'] == (single):
+                print("    - name: '%s'" % item['name'])
+                print("      email: '%s'" % item['email'])
+                print("      id: '%s'" % item['id'])
+            elif not full and not single:
+                print("      id: '%s'" % item['id'])
+
+    list_committers(full, single, project)
