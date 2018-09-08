0f6aefb9b71e20708f9045139cf0064bed3eb47c
Thanh Ha <thanh.ha@linuxfoundation.org>
2018-09-08 15:31:35 -0400

    Add cmd to reset API tokens in jenkins_jobs.ini
    Convenience command to automatically reset API tokens in
    jenkins-jobs.ini.
    
    Change-Id: I6d84760368b6a12e9e9622b5ce9496a278321dc4
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- lftools/cli/jenkins/token.py
+++ lftools/cli/jenkins/token.py
@@ -12,9 +12,14 @@
 __author__ = 'Thanh Ha'
 
 import logging
+import os
+import sys
 
 import click
+import requests
+from six.moves import configparser
 
+from lftools.jenkins import JJB_INI
 from lftools.jenkins.token import get_token
 
 log = logging.getLogger(__name__)
@@ -40,5 +45,41 @@ def print_token(ctx):
     log.info(get_token(ctx.obj['jenkins_url']))
 
 
+@click.command()
+def reset():
+    """Regenerate API tokens for all configurations in jenkins_jobs.ini."""
+    if not os.path.isfile(JJB_INI):
+        log.error('{} not found.'.format(JJB_INI))
+        sys.exit(1)
+
+    fail = 0
+    success = 0
+
+    config = configparser.ConfigParser()
+    config.read(JJB_INI)
+    for section in config.sections():
+        if not config.has_option(section, 'url'):
+            log.debug('Section does not contain a url, skipping...')
+            continue
+
+        url = config.get(section, 'url')
+        log.info('Resetting API key for {}'.format(url))
+        try:
+            token = get_token(url, True)
+            config.set(section, 'password', token)
+            success += 1
+        except requests.exceptions.ConnectionError as e:
+            log.error('Failed to reset API key for {}'.format(url))
+            fail += 1
+
+    with open(JJB_INI, 'w') as configfile:
+        config.write(configfile)
+
+    log.info('Update configurations complete.')
+    log.info('Success: {}'.format(success))
+    log.info('Failed: {}'.format(fail))
+
+
 token.add_command(change)
 token.add_command(print_token)
+token.add_command(reset)
