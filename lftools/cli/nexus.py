39865d4a9902ade329a09f90fb2456e27c5a0c49
Eric Ball <eball@linuxfoundation.org>
2018-09-12 17:36:16 -0700

    Add docker list/delete subcommands for nexus
    This commit adds a "docker" subcommand to "nexus", with options to
    list or delete images. This is intended for docker repos in Nexus 3.
    
    Also includes logic in the Nexus class to find the version and
    correct API base path from the provided URL.
    
    Issue-ID: RELENG-1164
    Change-Id: I759297ed9f49566df6062b2a158e0954673e29eb
    Signed-off-by: Eric Ball <eball@linuxfoundation.org>

--- lftools/cli/nexus.py
+++ lftools/cli/nexus.py
@@ -53,3 +53,46 @@ def create(ctx):
 def repo(ctx, config, settings):
     """Create a Nexus repository as defined by a repo-config.yaml file."""
     nexuscmd.create_repos(config, settings)
+
+
+@nexus.group()
+@click.pass_context
+def docker(ctx):
+    """Docker repos in Nexus."""
+    pass
+
+
+def search_options(command):
+    """Common options for all image_search subcommands."""
+    command = click.option(
+        '-s', '--settings', type=str, required=True,
+        help='Yaml file containing "nexus" (url), "user", and "password" '
+        + 'definitions.')(command)
+    command = click.option(
+        '-r', '--repo', type=str, required=True,
+        help='Repository to search.')(command)
+    command = click.option(
+        '-p', '--pattern', type=str, required=True,
+        help="Pattern to search for (use '!' for 'not').")(command)
+    return command
+
+
+@docker.command()
+@search_options
+@click.option(
+    '--csv', type=str, metavar="PATH",
+    help='Write a csv file of the search results to PATH.')
+@click.pass_context
+def list_images(ctx, settings, repo, pattern, csv):
+    """List images matching the pattern."""
+    nexuscmd.search("list", settings, repo, pattern, csv)
+
+
+@docker.command()
+@search_options
+@click.option(
+    '-y', '--yes', is_flag=True, help="Answer yes to all prompts")
+@click.pass_context
+def delete_images(ctx, settings, repo, pattern, yes):
+    """Delete all images matching the pattern."""
+    nexuscmd.search("delete", settings, repo, pattern, yes)
