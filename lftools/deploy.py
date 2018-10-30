58a82cb90644cd5fe4476041841f782b1edbe043
Suresh Channamallu <schannamallu@linuxfoundation.org>
2018-10-30 18:05:55 -0400

    Refactor nexus function to pure Python
    Converting the existing deploy bash script functions to python file.
    
    Issue: RELENG-1374
    Change-Id: Ic35cba006ce80a8a561104601b2a7ade601ac8ff
    Signed-off-by: Suresh Channamallu <schannamallu@linuxfoundation.org>

--- lftools/deploy.py
+++ lftools/deploy.py
@@ -408,3 +408,33 @@ def nexus_stage_repo_close(nexus_url, staging_profile_id, staging_repo_id):
 
     if not resp.status_code == 201:
         _log_error_and_exit("Failed with status code {}".format(resp.status_code), resp.text)
+
+
+    def upload_maven_file_to_nexus(nexus_url, nexus_repo_id, group_id, artifact_id, version, packaging, file,
+                                   classifier=None):
+
+        # This function is about uploading the maven file to nexus
+
+        # Declaring Params
+        params = ("-F r=" + nexus_repo_id)
+        params += ("-F g=" + group_id)
+        params += ("-F a=" + artifact_id)
+        params += ("-F v=" + version)
+        params += ("-F p=" + packaging)
+
+        if classifier:
+            params += ("-F c=" + classifier)
+        params += ("-F file=@" + file)
+
+        input_url = '{0}/service/local/artifact/maven/content'.format(nexus_url)
+
+        input_header = {'Content-Type': 'application/xml'}
+
+        response = _request_post(url=input_url, data=params, headers=input_header)
+
+        if response.status_code == "200":
+            return 0  # Pass
+        else:
+            print(response.status_code)
+            _log_error_and_exit(
+                "failed with status code:{}, status message: []".format(response.status_code, response.text))  # Fail
