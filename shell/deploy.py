492d68cc7ae72224eac85dea3c72f0261e77b530
Suresh Channamallu <schannamallu@linuxfoundation.org>
2018-10-30 18:16:02 -0400

    Added upload maven file to nexus functionality
    Converting the existing deploy bash script functions to python file.
    
    Issue: RELENG-1373
    Change-Id: Id3d459ac2fd1124d2504a3fb8ed7c23d49555fb1
    Signed-off-by: Suresh Channamallu <schannamallu@linuxfoundation.org>

---
+++ shell/deploy.py
@@ -0,0 +1,33 @@
+from io import BytesIO
+from xml.etree import ElementTree as ET
+import requests
+
+class NexusRepo:
+
+    def upload_maven_file_to_nexus(self,nexus_url,nexus_repo_id,group_id,artifact_id,version,packaging,file,classifier):
+
+        # This function is about uploading the maven file to nexus
+        # Declaring Params
+        params = ("-F r="+nexus_repo_id)
+        params += ("-F g="+group_id)
+        params += ("-F a="+artifact_id)
+        params += ("-F v="+version)
+        params += ("-F p="+packaging)
+
+        if classifier:
+            params += ("-F c="+classifier)
+        params += ("-F file=@"+file)
+
+        
+        input_url = '{0}/service/local/artifact/maven/content'.format(nexus_url)
+
+        input_header = {'Content-Type': 'application/xml'}
+
+        response = requests.post(url=input_url,data=params,headers=input_header)
+
+        if response.status_code == "200":
+            print("File Uploaded")
+        else:
+            print("Failed to upload")
+
+
