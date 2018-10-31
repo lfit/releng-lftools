515a4d5871312e242e81e268cb9ec16b05b5fd82
Thanh Ha <thanh.ha@linuxfoundation.org>
2018-10-31 10:38:27 -0400

    Refactor copy_archives deploy function to Python
    Issue: RELENG-1375
    Change-Id: I264c29f9a3b86415fd879ef9dce38714105309f0
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

---
+++ lftools/deploy.py
@@ -0,0 +1,59 @@
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2017 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+"""Library of functions for deploying artifacts to Nexus."""
+
+import glob
+import logging
+import os
+import shutil
+
+
+log = logging.getLogger(__name__)
+
+
+def copy_archives(workspace, pattern=None):
+    """Copy files matching PATTERN in a WORKSPACE to the current directory.
+
+    The best way to use this function is to cd into the directory you wish to
+    store the files first before calling the function.
+    """
+    archives_dir = os.path.join(workspace, 'archives')
+    dest_dir = os.getcwd()
+
+    log.debug('Copying files from {} with pattern \'{}\' to {}.'.format(
+        workspace, pattern, dest_dir))
+    for file_or_dir in os.listdir(archives_dir):
+        f = os.path.join(archives_dir, file_or_dir)
+        try:
+            log.debug('Moving {}.'.format(f))
+            shutil.move(f, dest_dir)
+        except shutil.Error as e:
+            log.warn(e)
+
+    paths = []
+    for p in pattern:
+        search = os.path.join(workspace, p)
+        paths.extend(glob.glob(search, recursive=True))
+    log.debug('Files found: {}'.format(paths))
+
+    for src in paths:
+        if len(os.path.basename(src)) > 255:
+            log.warn('Filename {} is over 255 characters. Skipping...'.format(
+                os.path.basename(src)))
+
+        dest = os.path.join(dest_dir, src[len(workspace)+1:])
+        log.debug('{} -> {}'.format(src, dest))
+
+        try:
+            shutil.move(src, dest)
+        except FileNotFoundError as e:
+            log.debug(e)
+            os.makedirs(os.path.dirname(dest))
+            shutil.move(src, dest)
