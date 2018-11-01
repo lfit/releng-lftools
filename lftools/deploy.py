74c96e29018e4a06e1627f28f61899fe72488410
Bengt Thuree <bthuree@linuxfoundation.org>
2018-11-01 19:20:12 -0400

    Refactor Deploy Nexus Zip command to Python
    Refactor the deploy nexus zip command for better portability with
    Windows systems.
    
    Issue: RELENG-1372
    Change-Id: I58ea1d7703b626f791dcd74e63251c4f3261ca7d
    Co-Authored-By: Thanh Ha <thanh.ha@linuxfoundation.org>
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- lftools/deploy.py
+++ lftools/deploy.py
@@ -65,3 +65,68 @@ def copy_archives(workspace, pattern=None):
             log.debug(e)
             os.makedirs(os.path.dirname(dest))
             shutil.move(src, dest)
+
+
+def deploy_nexus_zip (nexus_url, nexus_repo, nexus_path, deploy_zip):
+    """"Deploy zip file containing artifacts to Nexus using requests.
+
+    This function simply takes a zip file preformatted in the correct
+    directory for Nexus and uploads to a specified Nexus repo using the
+    content-compressed URL.
+
+    Requires the Nexus Unpack plugin and permission assigned to the upload user.
+
+    Parameters:
+    nexus_url:    URL to Nexus server. (Ex: https://nexus.opendaylight.org)
+    nexus_repo:   The repository to push to. (Ex: site)
+    nexus_path:   The path to upload the artifacts to. Typically the
+                  project group_id depending on if a Maven or Site repo
+                  is being pushed.
+                  Maven Ex: org/opendaylight/odlparent
+                  Site Ex: org.opendaylight.odlparent
+    deploy_zip:   The zip to deploy. (Ex: /tmp/artifacts.zip)
+    """
+
+    if len(nexus_url) == 0 or
+        len(nexus_repo) == 0 or
+        len(nexus_path) == 0 or
+        len(deploy_zip) == 0:
+        log.error("Not enough parameters")
+        sys.exit(1)
+
+    nexus_url = '{0}service/local/repositories/{1}/content-compressed/{2}'.format(
+        local_format_url(nexus_url),
+        nexus_repo,
+        nexus_path)
+
+    log.debug("nexus_url  = {}".format(nexus_url))
+    log.debug("deploy_zip = {}".format(deploy_zip))
+
+    fileobj = open(deploy_zip, 'rb')
+    files = {'file': fileobj}
+    try:
+        resp = requests.post(nexus_url, files=files)
+    except Exception as e:
+        log.error("Not valid nexus URL: {}".format(nexus_url))
+        log.error(e)
+        sys.exit(1)
+    finally:
+        fileobj.close()
+
+    log.debug("resp.status_code = {}".format(resp.status_code))
+
+    if not str(resp.status_code).startswith('20'):
+        log.error("Failed with: {}".format(resp.text))
+        zfile = zipfile.ZipFile(deploy_zip)
+        log.error(zfile.infolist())
+        sys.exit(1)
+
+    url = '{}/service/local/repositories/logs/content-compressed/{}'.format(
+        nexus_url, nexus_path)
+    r = requests.post(url, files=upload_files)
+    log.debug('{}: {}'.format(r.status_code, r.text))
+
+    if r.status_code != 201:
+        log.error('Failed to upload to Nexus with status code {}. {}'.format(
+            r.status_code, r.content))
+        sys.exit(1)
