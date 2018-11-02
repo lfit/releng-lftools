2fe6dfc9dc97e8da78e6a73c800b636df57bea78
Bengt Thuree <bthuree@linuxfoundation.org>
2018-11-02 11:51:38 +1100

    Internal func _log_error_and_exit & _request_post
    Adds two internal functions
    _log_error_and_exist : prints out 0, 1 or 2 lines of text
                           and then exit with sys.exit(1)
    
    _request_post        : This is a wrapper around request.post
                           Handles three exceptions
                             ConnectionError, MissingSchema, InvalidURL
    
    Change-Id: If8a4d20054d958cbfb3ab651eae037a124420bd3
    Signed-off-by: Bengt Thuree <bthuree@linuxfoundation.org>

--- tests/test_deploy.py
+++ tests/test_deploy.py
@@ -10,11 +10,17 @@
 """Test deploy command."""
 
 import os
-
+import sys
 import pytest
+from requests.exceptions import ConnectionError, MissingSchema, InvalidURL
 
 from lftools import cli
 import lftools.deploy as deploy_sys
+try:
+    import mock
+except ImportError:
+    from unittest import mock
+import requests
 
 FIXTURE_DIR = os.path.join(
     os.path.dirname(os.path.realpath(__file__)),
@@ -36,6 +42,13 @@ def test_format_url():
 
     for url in test_url:
         assert deploy_sys._format_url(url[0]) == url[1]
+
+
+def test_log_and_exit():
+    """Test exit."""
+    with pytest.raises(SystemExit) as excinfo:
+        deploy_sys._log_error_and_exit("testmsg")
+    assert excinfo.type == SystemExit
 
 
 @pytest.mark.datafiles(
@@ -78,3 +91,57 @@ def test_copy_archive_pattern(cli_runner, datafiles):
     assert os.path.exists(os.path.join(
         stage_dir, 'aaa', 'aaa-cert', 'target', 'surefire-reports',
         'org.opendaylight.aaa.cert.test.AaaCertMdsalProviderTest-output.txt'))
+
+def mocked_log_error(msg1=None, msg2=None):
+    """Mock local_log_error_and_exit function.
+    This function is modified to simply raise an Exception.
+    The original will print msg1 & msg2, then call sys.exit(1)."""
+    if 'Could not connect to URL:' in msg1:
+        raise ValueError('connection_error')
+    if 'Invalid URL:' in msg1:
+        raise ValueError('invalid_url')
+    if 'Not valid URL:' in msg1:
+        raise ValueError('missing_schema')
+    raise ValueError('fail')
+
+
+def mocked_requests_post(*args, **kwargs):
+    """Mock requests.post function."""
+    class MockResponse:
+        def __init__(self, json_data, status_code):
+            self.json_data = json_data
+            self.status_code = status_code
+            self.text = json_data
+
+        def json(self):
+            return self.json_data
+
+    if 'connection.error.test' in args[0]:
+        raise requests.exceptions.ConnectionError
+    if 'invalid.url.test' in args[0]:
+        raise requests.exceptions.InvalidURL
+    if 'missing.schema.test' in args[0]:
+        raise requests.exceptions.MissingSchema
+    return MockResponse(None, 404)
+
+@mock.patch('requests.post', side_effect=mocked_requests_post)
+@mock.patch('lftools.deploy._log_error_and_exit', side_effect=mocked_log_error)
+def test_local_request_post(self, mock_post):
+    """Test local_request_post."""
+    xml_doc="""
+        <promoteRequest>
+            <data>
+                <stagedRepositoryId>test1-1027</stagedRepositoryId>
+                <description>Close staging repository.</description>
+            </data>
+        </promoteRequest>
+        """
+    with pytest.raises(ValueError) as excinfo:
+        deploy_sys._request_post('connection.error.test', xml_doc, "{'Content-Type': 'application/xml'}")
+    assert 'connection_error' in str(excinfo.value)
+    with pytest.raises(ValueError) as excinfo:
+        deploy_sys._request_post('invalid.url.test:8081nexus', xml_doc, "{'Content-Type': 'application/xml'}")
+    assert 'invalid_url' in str(excinfo.value)
+    with pytest.raises(ValueError) as excinfo:
+        deploy_sys._request_post('http:/missing.schema.test:8081nexus', xml_doc, "{'Content-Type': 'application/xml'}")
+    assert 'missing_schema' in str(excinfo.value)
