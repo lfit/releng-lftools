6724b123178bca2c6d8c9b0aef6c541cb182f205
Thanh Ha <thanh.ha@linuxfoundation.org>
2018-11-08 10:31:30 +0800

    Refactor deploy-logs cmd to Python
    Issue: RELENG-1370
    Change-Id: Ibf58a92fe8800ad77d8be32a123da603c6a36c56
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- lftools/cli/deploy.py
+++ lftools/cli/deploy.py
@@ -128,11 +128,10 @@ def logs(ctx, nexus_url, nexus_path, build_url):
     for log archiving.
 
     To use this script the Nexus server must have a site repository configured
-    with the name "logs" as this is a hardcoded path. Also this script uses
-    ~/.netrc for it's authentication which must be provided.
+    with the name "logs" as this is a hardcoded path.
     """
-    status = subprocess.call(['deploy', 'logs', nexus_url, nexus_path, build_url])
-    sys.exit(status)
+    deploy_sys.deploy_logs(nexus_url, nexus_path, build_url)
+    log.info('Logs upload complete.')
 
 
 @click.command(name='maven-file')
