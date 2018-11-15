4190d92902b6015e5cfc01415132a8c31cfd6a5f
Eric Ball <eball@linuxfoundation.org>
2018-11-15 00:00:24 -0800

    Refactor deploy_maven_file from shell to python
    Refactoring the old shell script into pure python in order to
    improve compatibility with Windows.
    
    Issue: RELENG-1378
    Change-Id: I4b9efbdd43fdc6605c5e152ca1a2a6b132770d1d
    Signed-off-by: Eric Ball <eball@linuxfoundation.org>

--- lftools/nexus/cmd.py
+++ lftools/nexus/cmd.py
@@ -14,6 +14,7 @@ import csv
 import logging
 import sys
 
+import requests
 import yaml
 
 from lftools import config
@@ -39,8 +40,8 @@ def get_credentials(settings_file, url=None):
         elif set(['nexus', 'user', 'password']).issubset(settings):
             return settings
     elif url:
-        user = config.get_setting("global", "username")
-        password = config.get_setting("global", "password")
+        user = config.get_setting("nexus", "username")
+        password = config.get_setting("nexus", "password")
         return {"nexus": url, "user": user, "password": password}
     log.error('Please define a settings.yaml file, or include a url if using '
               + 'lftools.ini')
@@ -271,3 +272,81 @@ def delete_images(settings_file, url, images):
 
     for image in images:
         _nexus.delete_image(image)
+
+
+def deploy_maven_file(nexus_url, nexus_repo_id, file_name, pom_file="",
+                      group_id="", artifact_id="", packaging="", version="",
+                      classifier=""):
+    """Deploy a file to a Nexus maven2 repository.
+
+    :arg str nexus_url: URL of target Nexus server.
+    :arg str nexus_repo_id: Name of target Nexus repo.
+    :arg str file_name: Path to file that will be uploaded to Nexus.
+    :arg str pom_file: Optional path to POM file containing package info.
+    :arg str group_id: The artifact's groupId. Necessary if no pom is provided.
+    :arg str artifact_id: The artifact's artifactId. Necessary if no pom is
+        provided.
+    :arg str packaging: The artifact's packaging scheme. If not provided, this
+        will be guessed based on the file's extension.
+    :arg str version: The artifact's version. Necessary if no pom is provided.
+    :arg str classifier: Optional Nexus classifier for the artifact.
+    """
+    _nexus = Nexus(nexus_url)
+
+    if pom_file:
+        files = [
+            ('file', (pom_file, open(pom_file, 'rb'))),
+            ('file', (file_name, open(file_name, 'rb')))
+        ]
+
+        data = {
+            'r': (None, nexus_repo_id),
+            'hasPom': (None, 'true')
+        }
+
+        request_url = "{}/artifact/maven/content".format(_nexus.baseurl)
+        response = requests.post(request_url, data=data, files=files)
+
+    else:
+        packaging = util.find_packaging_from_file_name(file_name)
+
+        info_dict = {}
+        if packaging is "rpm":
+            info_dict = util.get_info_from_rpm(file_name)
+        elif packaging is "deb":
+            info_dict = util.get_info_from_deb(file_name)
+        if info_dict and not artifact_id:
+            artifact_id = info_dict["name"]
+        if info_dict and not version:
+            version = info_dict["version"]
+
+        data = {
+            'r': (None, nexus_repo_id),
+            'p': (None, packaging),
+            'hasPom': (None, 'false')
+        }
+
+        files = {
+            'file': (file_name, open(file_name, 'rb'))
+        }
+
+        if group_id:
+            data['g'] = (None, group_id)
+        if artifact_id:
+            data['a'] = (None, artifact_id)
+        if version:
+            data['v'] = (None, version)
+        if classifier:
+            data['c'] = (None, classifier)
+
+        request_url = "{}/artifact/maven/content".format(_nexus.baseurl)
+        log.debug("Request URL: {}".format(request_url))
+        response = requests.post(request_url, data=data, files=files)
+
+    if response.status_code != 201:
+        raise requests.HTTPError("Upload failed with the following "
+                                 "error:\n{}: {}".format(response.status_code,
+                                                         response.text))
+    else:
+        log.debug("Successfully uploaded {} to {}".format(file_name,
+                                                          request_url))
--- lftools/nexus/util.py
+++ lftools/nexus/util.py
@@ -13,6 +13,11 @@
 __author__ = 'Thanh Ha'
 
 import logging
+import re
+import sys
+
+from deb_pkg_tools.package import inspect_package_fields
+import rpmfile
 
 log = logging.getLogger(__name__)
 
@@ -20,3 +25,40 @@ log = logging.getLogger(__name__)
 def create_repo_target_regex(group_id):
     """Create a repo_target for Nexus use."""
     return '^/{}/.*'.format(group_id.replace('.', '[/\.]'))
+
+
+def find_packaging_from_file_name(file_name):
+    """Find packaging type based on file extension."""
+    if file_name.endswith(".tar.gz"):
+        packaging = "tar.gz"
+    else:
+        # Find packaging based on file extension
+        ext_index = file_name.rfind(".")
+        if ext_index == -1:
+            log.error("ERROR: Could not determine packaging type. Please "
+                      "provide an appropriate \"packaging\" parameter.")
+            sys.exit(1)
+        packaging = file_name[ext_index+1:]
+    return packaging
+
+
+def get_info_from_rpm(path):
+    """Return data pulled from the headers of an RPM file."""
+    info = {}
+    rpm = rpmfile.open(path)
+    info["name"] = rpm.headers.get("name")
+    ver = rpm.headers.get("version")
+    if re.search('\.s(rc\.)?rpm'):
+        info["version"] = "{}.{}".format(ver, "src")
+    else:
+        info["version"] = "{}.{}".format(ver, rpm.headers.get("arch"))
+    return info
+
+
+def get_info_from_deb(path):
+    """Return data pulled from a deb archive."""
+    info = {}
+    data = inspect_package_fields(path)
+    info["name"] = data["Package"]
+    info["version"] = data["Version"]
+    return info
