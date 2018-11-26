c3bd094403b7560d6c163c5dd367c5000c238c15
Aric Gardner <agardner@linuxfoundation.org>
2018-11-26 15:48:48 -0500

    lftools gerrit create for new projects
    $ lftools gerrit create --help
    Usage: lftools gerrit create [OPTIONS] GERRIT_URL LDAP_GROUP REPO USER
      Create and configure permissions for a new gerrit repo.
      GERRIT_URL: gerrit.localhost
      LDAP_GROUP: project-gerrit-group-committers
      REPO: testrepo
      USER: user that has permissions in gerrit
    
    Creates a project and sets up permissions
    
        calls gerrit create project:
    
    ssh -p 29418 $user@$project gerrit create-project $repository
    --empty-commit --parent All-Projects
    
        creates a group file:
    
    4b0ee50b1dda533aa89c224e0234b35e92d2e49b        GitHub Replication
    global:Registered-Users Registered Users
    ldap:cn=opnfv-gerrit-test-submitters,ou=Groups,dc=freestandards,dc=org\
     ldap/opnfv-gerrit-test-submitters
    
        sets up permissions:
    
    project.config 'access.refs/*.read' "deny group GitHub Replication"
    project.config 'access.refs/*.owner' "group ldap/$ldapgroup"
    
    Current plan is to add the git review and skeleton for the docs
     in a diffrent command.
    
    Issue: RELENG-1524
    
    Change-Id: Id5bb3c52918c5fcad9e10b32ee14a90eb0b0d3ff
    Signed-off-by: Aric Gardner <agardner@linuxfoundation.org>

---
+++ lftools/cli/gerrit.py
@@ -0,0 +1,55 @@
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2018 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+"""Create a gerrit project."""
+
+from __future__ import print_function
+
+import subprocess
+import sys
+
+import click
+
+
+@click.group()
+@click.pass_context
+def gerrit_cli(ctx):
+    """GERRIT TOOLS."""
+    pass
+
+
+@click.command()
+@click.argument('gerrit_url')
+@click.argument('ldap_group')
+@click.argument('repo')
+@click.argument('user')
+@click.pass_context
+def create(
+        ctx, gerrit_url, ldap_group, repo, user):
+    """Create and configure permissions for a new gerrit repo.
+
+    GERRIT_URL: gerrit.localhost
+
+    LDAP_GROUP: project-gerrit-group-committers
+
+    REPO: testrepo
+
+    USER: user that has permissions in gerrit
+    """
+    params = ['gerrit_create']
+    params.extend(["-p", gerrit_url])
+    params.extend(["-d", ldap_group])
+    params.extend(["-r", repo])
+    params.extend(["-u", user])
+
+    status = subprocess.call(params)
+    sys.exit(status)
+
+
+gerrit_cli.add_command(create)
