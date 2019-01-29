ee07385ac77ff4a8533fe1f68b4239a6e9ec9ab0
Aric Gardner <agardner@linuxfoundation.org>
2019-01-29 15:25:44 -0500

    Add "schema validate" command
    This command will validate a yaml file
    based on a schema file
    
    ISSUE: RELENG-1685
    
    Change-Id: I59d427ff52ff61be3df9b1413828ff13b814653a
    Signed-off-by: Aric Gardner <agardner@linuxfoundation.org>

---
+++ lftools/schema.py
@@ -0,0 +1,46 @@
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2019 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+"""Verify YAML Schema."""
+
+from __future__ import print_function
+
+import logging
+
+import jsonschema
+import yaml
+
+
+def check_schema_file(yamlfile, schemafile):
+    """Verify YAML Schema.
+
+    YAMLFILE: Release YAML file to be validated.
+
+    SCHEMAFILE: SCHEMA file to validate against.
+    """
+    with open(yamlfile) as _:
+        yaml_file = yaml.safe_load(_)
+
+    with open(schemafile) as _:
+        schema_file = yaml.safe_load(_)
+
+    # Load the schema
+    validation = jsonschema.Draft4Validator(
+        schema_file,
+        format_checker=jsonschema.FormatChecker()
+    )
+
+    validation.iter_errors(yaml_file)
+    # Look for errors
+    errors = 0
+    for error in validation.iter_errors(yaml_file):
+        errors += 1
+        logging.error(error)
+    if errors > 0:
+        raise RuntimeError("{:d} issues invalidate the release schema".format(errors))
---
+++ tests/test_schema.py
@@ -0,0 +1,39 @@
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2019 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+"""Test license command."""
+
+import os
+
+import pytest
+
+from lftools import cli
+
+FIXTURE_DIR = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)),
+    'fixtures',
+    )
+
+
+@pytest.mark.datafiles(
+    os.path.join(FIXTURE_DIR, 'schema'),
+    )
+def test_check_license(cli_runner, datafiles):
+    """Test check_schema() command."""
+    os.chdir(str(datafiles))
+
+    # Check that schema passes when schema and yaml are valid.
+    result = cli_runner.invoke(cli.cli, ['schema', 'verify', 'release.yaml', 'schema.yaml'], obj={})
+    # noqa: B101 .
+    assert result.exit_code == 0
+
+    # Check that schema fails when schema and yaml are invalid.
+    result = cli_runner.invoke(cli.cli, ['schema', 'verify', 'release-broken.yaml', 'schema.yaml'], obj={})
+    # noqa: B101 .
+    assert result.exit_code == 1
