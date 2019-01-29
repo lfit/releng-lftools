62c2224c41dc0ba1fb6f119a722b6ec4e9f12e43
Aric Gardner <agardner@linuxfoundation.org>
2019-01-29 15:25:44 -0500

    Add "schema validate" command
    This command will validate a yaml file
    based on a schema file
    
    ISSUE: RELENG-1685
    
    Change-Id: I59d427ff52ff61be3df9b1413828ff13b814653a
    Signed-off-by: Aric Gardner <agardner@linuxfoundation.org>

---
+++ lftools/cli/schema.py
@@ -0,0 +1,40 @@
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2019 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+"""Verify YAML Schema."""
+
+from __future__ import print_function
+
+import click
+
+from lftools.schema import check_schema_file
+
+
+@click.group()
+@click.pass_context
+def schema(ctx):
+    """Verify YAML Schema."""
+    pass
+
+
+@click.command(name='verify')
+@click.argument('yamlfile')
+@click.argument('schemafile')
+@click.pass_context
+def verify_schema(ctx, yamlfile, schemafile):
+    """Verify YAML Schema.
+
+    YAMLFILE: Release YAML file to be validated.
+
+    SCHEMAFILE: SCHEMA file to validate against.
+    """
+    check_schema_file(yamlfile, schemafile)
+
+
+schema.add_command(verify_schema)
