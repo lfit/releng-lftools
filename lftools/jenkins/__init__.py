896e5f3d1bec11bfc64f256afe1d7eaef009e185
Anil Belur <abelur@linuxfoundation.org>
2019-06-10 14:23:31 +1000

    Handle multiple sections of Jenkins configuration
    Handle config parser conditionally, when multiple Jenkins server
    sections are available in the jenkins.ini file.
    
    Runnning `tox -e docs` returns an error when the ``jenkins.ini``
    file has more than one server section available in the config file.
    
    Use SafeConfigParser which is a saner version for parsing
    config files.
    
    Issue: RELENG-2120
    Change-Id: I274ac46cf7a1d03cf7c3c6fdaec6094b5e02aa42
    Signed-off-by: Anil Belur <abelur@linuxfoundation.org>

--- lftools/jenkins/__init__.py
+++ lftools/jenkins/__init__.py
@@ -58,11 +58,18 @@ class Jenkins():
         if '://' not in server:
             if self.config_file:
                 log.debug('Using config from {}'.format(self.config_file))
-                config = configparser.ConfigParser()
-                config.read(self.config_file)
-                user = config.get(server, 'user')
-                password = config.get(server, 'password')
-                server = config.get(server, 'url')
+                parser = configparser.SafeConfigParser()
+                parser.read(self.config_file)
+
+                if server not in parser.sections():
+                    log.debug('[{}] section found in {}'.format(server, self.config_file))
+                    for section in parser.sections():
+                        log.debug('section found: [{}]'.format(section))
+                    log.debug('set section with -s|--server')
+                else:
+                    user = parser.get(server, 'user')
+                    password = parser.get(server, 'password')
+                    server = parser.get(server, 'url')
             else:
                 log.debug('jenkins_jobs.ini not found in any of the default paths.')
                 server = 'https://localhost:8080'
