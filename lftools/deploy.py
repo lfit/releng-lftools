85f7c5f440c67d80d1d728ca9aac10d58a4d87e4
Eric Ball <eball@linuxfoundation.org>
2019-06-12 15:14:48 -0700

    Remove str casting of six.text_type return
    six.text_type is returning a properly-formatted string. Keeping the
    str() cast in the code only served to cause problems in Python 2.
    
    Change-Id: Idfd89778f9423c9d45806a7920c30d682937da11
    Signed-off-by: Eric Ball <eball@linuxfoundation.org>

--- lftools/deploy.py
+++ lftools/deploy.py
@@ -310,11 +310,11 @@ def deploy_logs(nexus_url, nexus_path, build_url):
 
     resp = requests.get('{}/consoleText'.format(_format_url(build_url)))
     with open('console.log', 'w+') as f:
-        f.write(str(six.text_type(resp.text.split(MAGIC_STRING)[0])))
+        f.write(six.text_type(resp.text.split(MAGIC_STRING)[0]))
 
     resp = requests.get('{}/timestamps?time=HH:mm:ss&appendLog'.format(_format_url(build_url)))
     with open('console-timestamp.log', 'w+') as f:
-        f.write(str(six.text_type(resp.text.split(MAGIC_STRING)[0])))
+        f.write(six.text_type(resp.text.split(MAGIC_STRING)[0]))
 
     _compress_text(work_dir)
 
