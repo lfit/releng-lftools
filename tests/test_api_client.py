48aac0afada51c43f110137cac956aae4d2b83af
DW Talton <dtalton@contractor.linuxfoundation.org>
2019-08-28 18:29:37 -0700

    Add support for Read the Docs API v3
    Add support for RTD APIv3, which allows for PUT and POST
    operations (v2 was read-only). Support added via modular
    API client with RTD module
    
    Issue: RELENG-2239
    Signed-off-by: DW Talton <dtalton@contractor.linuxfoundation.org>
    Change-Id: Iaad90ed360eaac0de0e7a6c5a4c6fa77ab349f70

---
+++ tests/test_api_client.py
@@ -0,0 +1,58 @@
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2018 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+"""Test generic REST client."""
+
+import responses
+
+import lftools.api.client as client
+
+c = client.RestApi(endpoint='', token='xyz')
+
+
+@responses.activate
+def test_get():
+    responses.add(responses.GET, 'https://fakeurl/', json={'success': 'get'},
+                  status=200, match_querystring=True)
+    resp = c.get('https://fakeurl/')
+    assert resp[1] == {'success': 'get'}
+
+
+@responses.activate
+def test_patch():
+    responses.add(responses.PATCH, url='https://fakeurl/',
+                  json={'success': 'patch'}, status=204,
+                  match_querystring=True)
+    resp = c.patch('https://fakeurl/')
+    assert resp[1] == {'success': 'patch'}
+
+
+@responses.activate
+def test_post():
+    responses.add(responses.POST, 'https://fakeurl/', json={'success': 'post'},
+                  status=201, match_querystring=True)
+    resp = c.post('https://fakeurl/')
+    assert resp[1] == {'success': 'post'}
+
+
+@responses.activate
+def test_put():
+    responses.add(responses.PUT, 'https://fakeurl/', json={'success': 'put'},
+                  status=200, match_querystring=True)
+    resp = c.put('https://fakeurl/')
+    assert resp[1] == {'success': 'put'}
+
+
+@responses.activate
+def test_delete():
+    responses.add(responses.DELETE, 'https://fakeurl/',
+                  json={'success': 'delete'}, status=200,
+                  match_querystring=True)
+    resp = c.delete('https://fakeurl/')
+    assert resp[1] == {'success': 'delete'}
