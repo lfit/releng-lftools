1a42472c9282bcb033cd6af46854e743c32418a2
Houa Yang <hyang@contractor.linuxfoundation.org>
2019-09-24 15:51:50 -0500

    Add S3 log shipping subcommand
    Add new click command for logs-s3.
    
    Issue-Id: RELENG-2379, RELENG-2380
    Signed-off-by: Houa Yang <hyang@contractor.linuxfoundation.org>
    Change-Id: I4295272cd903c9cc33962f2ee54667c28c3132e0

--- lftools/deploy.py
+++ lftools/deploy.py
@@ -17,7 +17,7 @@ import gzip
 import io
 import logging
 import math
-from multiprocessing import cpu_count
+import mimetypes
 import os
 import re
 import shutil
@@ -26,11 +26,14 @@ import sys
 import tempfile
 import zipfile
 
+import boto3
+from botocore.exceptions import ClientError
 from defusedxml.minidom import parseString
 import requests
 import six
 
 log = logging.getLogger(__name__)
+logging.getLogger("botocore").setLevel(logging.CRITICAL)
 
 
 def _compress_text(dir):
@@ -401,6 +404,180 @@ def deploy_logs(nexus_url, nexus_path, build_url):
 
     os.chdir(previous_dir)
     shutil.rmtree(work_dir)
+
+
+def deploy_s3(s3_bucket, s3_path, build_url, workspace, pattern=None):
+    """Add logs and archives to temp directory to be shipped to S3 bucket.
+
+    Fetches logs and system information and pushes them and archives to S3
+    for log archiving.
+
+    Requires the s3 bucket to exist.
+
+    Parameters:
+
+        :s3_bucket: Name of S3 bucket. Eg: lf-project-date
+        :s3_path: Path on S3 bucket place the logs and archives. Eg:
+            $SILO/$JENKINS_HOSTNAME/$JOB_NAME/$BUILD_NUMBER
+        :build_url: URL of the Jenkins build. Jenkins typically provides this
+                    via the $BUILD_URL environment variable.
+        :workspace: Directory in which to search, typically in Jenkins this is
+            $WORKSPACE
+        :pattern: Space-separated list of Globstar patterns of files to
+            archive. (optional)
+    """
+    work_dir = tempfile.mkdtemp(prefix="lftools-dl.")
+    os.chdir(work_dir)
+    previous_dir = os.getcwd()
+    s3_bucket = s3_bucket.lower()
+    previous_dir = os.getcwd()
+    s3 = boto3.resource("s3")
+
+    log.debug("work_dir: {}".format(work_dir))
+
+    # Copy archive files to tmp dir
+    copy_archives(workspace, pattern)
+    _compress_text(work_dir)
+
+    # Create build logs
+    build_details = io.open("_build-details.log", "w+")
+    build_details.write("build-url: {}".format(build_url))
+
+    with io.open("_sys-info.log", "w+") as sysinfo_log:
+        sys_cmds = []
+
+        log.debug("Platform: {}".format(sys.platform))
+        if sys.platform == "linux" or sys.platform == "linux2":
+            sys_cmds = [
+                ["uname", "-a"],
+                ["lscpu"],
+                ["nproc"],
+                ["df", "-h"],
+                ["free", "-m"],
+                ["ip", "addr"],
+                ["sar", "-b", "-r", "-n", "DEV"],
+                ["sar", "-P", "ALL"],
+            ]
+
+        for c in sys_cmds:
+            try:
+                output = subprocess.check_output(c).decode("utf-8")
+            except FileNotFoundError:
+                log.debug("Command not found: {}".format(c))
+                continue
+
+            output = "---> {}:\n{}\n".format(" ".join(c), output)
+            sysinfo_log.write(output)
+            log.info(output)
+
+    build_details.close()
+    sysinfo_log.close()
+
+    # Magic string used to trim console logs at the appropriate level during wget
+    MAGIC_STRING = "-----END_OF_BUILD-----"
+    log.info(MAGIC_STRING)
+
+    resp = requests.get("{}/consoleText".format(_format_url(build_url)))
+    with io.open("console.log", "w+", encoding="utf-8") as f:
+        f.write(six.text_type(resp.content.decode("utf-8").split(MAGIC_STRING)[0]))
+        f.close()
+
+    resp = requests.get("{}/timestamps?time=HH:mm:ss&appendLog".format(_format_url(build_url)))
+    with io.open("console-timestamp.log", "w+", encoding="utf-8") as f:
+        f.write(six.text_type(resp.content.decode("utf-8").split(MAGIC_STRING)[0]))
+        f.close()
+
+    _compress_text(work_dir)
+
+    def _s3_put_file(file):
+        if mimetypes.guess_type(file)[0] is None and mimetypes.guess_type(file)[1] is None:
+            try:
+                s3.Bucket(s3_bucket).upload_file(
+                    file, "{}{}".format(s3_path, file), ExtraArgs={"ContentType": "text/plain"}
+                )
+            except ClientError as e:
+                log.error(e)
+                return False
+            return True
+        elif mimetypes.guess_type(file)[0] is None:
+            try:
+                s3.Bucket(s3_bucket).upload_file(
+                    file,
+                    "{}{}".format(s3_path, file),
+                    ExtraArgs={"ContentType": "text/plain", "ContentEncoding": mimetypes.guess_type(file)[1]},
+                )
+            except ClientError as e:
+                log.error(e)
+                return False
+            return True
+        else:
+            try:
+                s3.Bucket(s3_bucket).upload_file(file, "{}{}".format(s3_path, file))
+            except ClientError as e:
+                log.error(e)
+                return False
+            return True
+
+    def _deploy_s3_upload(file):
+        log.info("Attempting to upload file {}".format(file))
+        if _s3_put_file(file):
+            return True
+        else:
+            return False
+
+    # Create file list to upload
+    file_list = []
+    files = glob.glob("**/*", recursive=True)
+    for file in files:
+        if os.path.isfile(file):
+            file_list.append(file)
+
+    # Create, upload & delete dummy files to create directory structure
+    s3_silo = s3_path.split("/")[0] + "/"
+    s3_jenkins_host = s3_silo + s3_path.split("/")[1] + "/"
+    log.info("Creating dummy file")
+    open("silo", "a").close()
+    open("jenkins_host", "a").close()
+    log.info("Uploading file {} to {}".format("silo", s3_silo))
+    s3.Bucket(s3_bucket).upload_file("silo", "{}{}".format(s3_silo, "silo"))
+    log.info("Uploading file {} to {}".format("jenkins_host", s3_jenkins_host))
+    s3.Bucket(s3_bucket).upload_file("jenkins_host", "{}{}{}".format(s3_silo, s3_jenkins_host, "jenkins_host"))
+
+    # Perform async upload
+    log.info("#######################################################")
+    log.info("Deploying files from {} to {}/{}".format(work_dir, s3_bucket, s3_path))
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
+        # this creates a dict where the key is the Future object, and the value
+        # is the file name
+        # see concurrent.futures.Future for more info
+        futures = {executor.submit(_deploy_s3_upload, file_name): file_name for file_name in file_list}
+        for future in concurrent.futures.as_completed(futures):
+            filename = futures[future]
+            try:
+                data = future.result()
+                # remove pyflake warning
+                if data == data:
+                    pass
+            except Exception as e:
+                log.error("Uploading {}: {}".format(filename, e))
+
+        # wait until all threads complete (successfully or not)
+        # then log the results of the upload threads
+        concurrent.futures.wait(futures)
+        for k, v in futures.items():
+            if k.result():
+                log.info("Successfully uploaded {}".format(v))
+            else:
+                log.error("FAILURE: Uploading {} failed".format(v))
+
+    log.info("Finished deploying from {} to {}/{}".format(work_dir, s3_bucket, s3_path))
+    log.info("#######################################################")
+
+    os.chdir(previous_dir)
+    shutil.rmtree(work_dir)
+    # s3.Object(s3_bucket, "{}{}".format(s3_silo, "test.txt")).delete()
+    # s3.Object(s3_bucket, "{}{}{}".format(s3_silo, s3_jenkins_host, "test.txt")).delete()
 
 
 def deploy_nexus_zip(nexus_url, nexus_repo, nexus_path, zip_file):
