7eb2b2ac07c31f16c61a229f82d10428c306a6fd
Houa Yang <hyang@contractor.linuxfoundation.org>
2019-09-24 15:51:50 -0500

    Add S3 log shipping subcommand
    Add new click command for logs-s3.
    
    Issue-Id: RELENG-2379, RELENG-2380
    Signed-off-by: Houa Yang <hyang@contractor.linuxfoundation.org>
    Change-Id: I4295272cd903c9cc33962f2ee54667c28c3132e0

--- lftools/cli/deploy.py
+++ lftools/cli/deploy.py
@@ -11,7 +11,6 @@
 
 __author__ = 'Thanh Ha'
 
-
 import logging
 import subprocess
 import sys
@@ -49,12 +48,12 @@ def archives(ctx, nexus_url, nexus_path, workspace, pattern):
 
     Provides 2 ways to archive files:
 
-        \b
         1) globstar pattern provided by the user.
         2) $WORKSPACE/archives directory provided by the user.
 
     To use this command the Nexus server must have a site repository configured
     with the name "logs" as this is a hardcoded log path.
+
     """
     if not pattern:
         pattern = None
@@ -96,15 +95,8 @@ def copy_archives(ctx, workspace, pattern):
 @click.argument('file')
 @click.option('-c', '--classifier', default='')
 @click.pass_context
-def file(ctx,
-         nexus_url,
-         nexus_repo_id,
-         group_id,
-         artifact_id,
-         version,
-         packaging,
-         classifier,
-         file):
+def file(ctx, nexus_url, nexus_repo_id, group_id, artifact_id, version,
+         packaging, classifier, file):
     """Upload file to Nexus as a Maven artifact using cURL.
 
     This function will upload an artifact to Nexus while providing all of
@@ -112,10 +104,9 @@ def file(ctx,
     specs.
     """
     try:
-        deploy_sys.upload_maven_file_to_nexus(
-            nexus_url, nexus_repo_id,
-            group_id, artifact_id, version,
-            packaging, file, classifier)
+        deploy_sys.upload_maven_file_to_nexus(nexus_url, nexus_repo_id,
+                                              group_id, artifact_id, version,
+                                              packaging, file, classifier)
     except HTTPError as e:
         log.error(str(e))
         sys.exit(1)
@@ -136,6 +127,7 @@ def logs(ctx, nexus_url, nexus_path, build_url):
 
     To use this script the Nexus server must have a site repository configured
     with the name "logs" as this is a hardcoded path.
+
     """
     try:
         deploy_sys.deploy_logs(nexus_url, nexus_path, build_url)
@@ -143,7 +135,27 @@ def logs(ctx, nexus_url, nexus_path, build_url):
         log.error(str(e))
         sys.exit(1)
 
-    log.info('Logs upload complete.')
+    log.info('Logs upload to Nexus complete.')
+
+
+@click.command(name='s3')
+@click.argument('s3_bucket', envvar='S3_BUCKET')
+@click.argument('s3_path')
+@click.argument('build-url', envvar='BUILD_URL')
+@click.argument('workspace', envvar='WORKSPACE')
+@click.option('-p', '--pattern', multiple=True)
+@click.pass_context
+def s3(ctx, s3_bucket, s3_path, build_url, workspace, pattern):
+    """Deploy logs and archives to a S3 bucket."""
+    if not pattern:
+        pattern = None
+    try:
+        deploy_sys.deploy_s3(s3_bucket, s3_path, build_url, workspace, pattern)
+    except HTTPError as e:
+        log.error(str(e))
+        sys.exit(1)
+
+    log.info('Logs upload to S3 complete.')
 
 
 @click.command(name='maven-file')
@@ -151,33 +163,43 @@ def logs(ctx, nexus_url, nexus_path, build_url):
 @click.argument('repo-id', envvar='REPO_ID')
 @click.argument('file-name', envvar='FILE_NAME')
 # Maven Config
-@click.option('-b', '--maven-bin', envvar='MAVEN_BIN',
-              help='Path of maven binary.')
-@click.option('-gs', '--global-settings', envvar='GLOBAL_SETTINGS_FILE',
-              help='Global settings file.')
-@click.option('-s', '--settings', envvar='SETTINGS_FILE',
-              help='Settings file.')
-@click.option('-p', '--maven-params',
-              help='Pass Maven commandline options to the mvn command.')
+@click.option(
+    '-b', '--maven-bin', envvar='MAVEN_BIN', help='Path of maven binary.')
+@click.option(
+    '-gs',
+    '--global-settings',
+    envvar='GLOBAL_SETTINGS_FILE',
+    help='Global settings file.')
+@click.option(
+    '-s', '--settings', envvar='SETTINGS_FILE', help='Settings file.')
+@click.option(
+    '-p',
+    '--maven-params',
+    help='Pass Maven commandline options to the mvn command.')
 # Maven Artifact GAV
-@click.option('-a', '--artifact-id',
-              help='Maven Artifact ID.')
-@click.option('-c', '--classifier',
-              help='File classifier.')
-@click.option('-f', '--pom-file',
-              help='Pom file to extract GAV information from.')
-@click.option('-g', '--group-id',
-              help='Maven Group ID')
-@click.option('-v', '--version',
-              help='Maven artifact version.')
+@click.option(
+    '-a', '--artifact-id', help='Maven Artifact ID.')
+@click.option('-c', '--classifier', help='File classifier.')
+@click.option(
+    '-f', '--pom-file', help='Pom file to extract GAV information from.')
+@click.option('-g', '--group-id', help='Maven Group ID')
+@click.option('-v', '--version', help='Maven artifact version.')
 @click.pass_context
 def maven_file(
-    # Maven Config
-    ctx, nexus_url, repo_id, file_name,
-    maven_bin, global_settings, settings,
-    maven_params,
-    # Maven GAV
-    artifact_id, group_id, classifier, version,
+        # Maven Config
+        ctx,
+        nexus_url,
+        repo_id,
+        file_name,
+        maven_bin,
+        global_settings,
+        settings,
+        maven_params,
+        # Maven GAV
+        artifact_id,
+        group_id,
+        classifier,
+        version,
         pom_file):
     """Deploy a file to a Nexus maven2 repository.
 
@@ -228,8 +250,12 @@ def maven_file(
 @click.command()
 @click.argument('nexus-repo-url', envvar='NEXUS_REPO_URL')
 @click.argument('deploy-dir', envvar='DEPLOY_DIR')
-@click.option('-s', '--snapshot', is_flag=True, default=False,
-              help='Deploy a snapshot repo.')
+@click.option(
+    '-s',
+    '--snapshot',
+    is_flag=True,
+    default=False,
+    help='Deploy a snapshot repo.')
 @click.pass_context
 def nexus(ctx, nexus_repo_url, deploy_dir, snapshot):
     """Deploy a Maven repository to a specified Nexus repository.
@@ -241,7 +267,8 @@ def nexus(ctx, nexus_repo_url, deploy_dir, snapshot):
 
         https://nexus.example.org/content/repositories/release
     """
-    log.debug("nexus_repo_url={}, deploy_dir={}, snapshot={}".format(nexus_repo_url, deploy_dir, snapshot))
+    log.debug("nexus_repo_url={}, deploy_dir={}, snapshot={}".format(
+        nexus_repo_url, deploy_dir, snapshot))
     try:
         deploy_sys.deploy_nexus(nexus_repo_url, deploy_dir, snapshot)
     except IOError as e:
@@ -261,9 +288,7 @@ def nexus_stage(ctx, nexus_url, staging_profile_id, deploy_dir):
     This script takes a local Maven repository and deploys it to a Nexus
     staging repository as defined by the staging-profile-id.
     """
-    deploy_sys.deploy_nexus_stage(nexus_url,
-                                  staging_profile_id,
-                                  deploy_dir)
+    deploy_sys.deploy_nexus_stage(nexus_url, staging_profile_id, deploy_dir)
 
 
 @click.command(name='nexus-stage-repo-close')
@@ -271,10 +296,10 @@ def nexus_stage(ctx, nexus_url, staging_profile_id, deploy_dir):
 @click.argument('staging-profile-id', envvar='STAGING_PROFILE_ID')
 @click.argument('staging-repo-id')
 @click.pass_context
-def nexus_stage_repo_close(ctx, nexus_url, staging_profile_id, staging_repo_id):
+def nexus_stage_repo_close(ctx, nexus_url, staging_profile_id,
+                           staging_repo_id):
     """Close a Nexus staging repo."""
-    deploy_sys.nexus_stage_repo_close(nexus_url,
-                                      staging_profile_id,
+    deploy_sys.nexus_stage_repo_close(nexus_url, staging_profile_id,
                                       staging_repo_id)
 
 
@@ -305,7 +330,8 @@ def nexus_zip(ctx, nexus_url, nexus_repo, nexus_path, deploy_zip):
     Requires the Nexus Unpack plugin and permission assigned to the upload user.
     """
     try:
-        deploy_sys.deploy_nexus_zip(nexus_url, nexus_repo, nexus_path, deploy_zip)
+        deploy_sys.deploy_nexus_zip(nexus_url, nexus_repo, nexus_path,
+                                    deploy_zip)
     except IOError as e:
         log.error(str(e))
         sys.exit(1)
@@ -326,3 +352,4 @@ deploy.add_command(nexus_stage)
 deploy.add_command(nexus_stage_repo_close)
 deploy.add_command(nexus_stage_repo_create)
 deploy.add_command(nexus_zip)
+deploy.add_command(s3)
