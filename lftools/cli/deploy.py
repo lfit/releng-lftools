b77b8b38ce551e3260f117940b5bac1a1317034d
Houa Yang <hyang@contractor.linuxfoundation.org>
2019-09-24 15:51:50 -0500

    Add s3 log shipping subcommand
    Add subcommand for deploy logs & archive to s3 bucket.
    
    Issue-Id:RELENG-2379, RELENG-2380
    
    Signed-off-by: Houa Yang <hyang@contractor.linuxfoundation.org>
    Change-Id: I4295272cd903c9cc33962f2ee54667c28c3132e0

--- lftools/cli/deploy.py
+++ lftools/cli/deploy.py
@@ -145,6 +145,77 @@ def logs(ctx, nexus_url, nexus_path, build_url):
 
     log.info('Logs upload complete.')
 
+@click.command(name='archives-s3')
+@click.argument('s3_bucket', envvar='S3_BUCKET')
+@click.argument('workspace', envvar='WORKSPACE')
+@click.option('-p', '--pattern', multiple=True)
+@click.pass_context
+def archives_s3(ctx, s3_bucket, workspace, pattern):
+    """Archive files to a s3 bucket.
+
+    Provides 2 ways to archive files:
+
+        1) globstar pattern provided by the user.
+        2) $WORKSPACE/archives directory provided by the user.
+
+    Requirements:
+
+    To use this API a must have a s3 bucket configured.
+
+    """
+    if not pattern:
+        pattern = None
+
+    try:
+        deploy_sys.deploy_archives_s3(s3_bucket, workspace, pattern)
+    except HTTPError as e:
+        log.error(str(e))
+        sys.exit(1)
+    except OSError as e:
+        deploy_sys._log_error_and_exit(str(e))
+
+    log.info('Archives upload complete.')
+
+@click.command(name='logs-s3')
+@click.argument('s3_bucket', envvar='S3_BUCKET')
+@click.argument('build-url', envvar='BUILD_URL', default=False)
+@click.pass_context
+def logs_s3(ctx, s3_bucket, build_url):
+    """Deploy logs to a S3 bucket.
+
+    This script fetches logs and system information and pushes them to s3
+    for log archiving.
+    """
+    try:
+        deploy_sys.deploy_logs_s3(s3_bucket, build_url)
+    except HTTPError as e:
+        log.error(str(e))
+        sys.exit(1)
+
+    log.info('Logs upload complete.')
+
+@click.command(name='s3-zip')
+@click.argument('s3_bucket', envvar='S3_BUCKET')
+@click.argument('deploy_zip', envvar='DEPLOY_ZIP')
+@click.pass_context
+def s3_zip(ctx, s3_bucket, deploy_zip):
+    """Deploy zip file to s3 using awscli/s3cmd.
+
+    This script simply takes a zip file and uploads to a specified s3 bucket using the
+    awscli/s3cmd.
+
+    Requires the awscli/s3cmd via pip and correct aws key_id/secret_key to the upload.
+    """
+    try:
+        deploy_sys.deploy_s3_zip(s3_bucket, deploy_zip)
+    except IOError as e:
+        log.error(str(e))
+        sys.exit(1)
+    except HTTPError as e:
+        log.error(str(e))
+        sys.exit(1)
+
+    log.info('Zip file upload complete.')
 
 @click.command(name='maven-file')
 @click.argument('nexus-url', envvar='NEXUS_URL')
@@ -326,3 +397,5 @@ deploy.add_command(nexus_stage)
 deploy.add_command(nexus_stage_repo_close)
 deploy.add_command(nexus_stage_repo_create)
 deploy.add_command(nexus_zip)
+deploy.add_command(s3_zip)
+deploy.add_command(logs_s3)
