c40039583891448457e53e05d245abf76d46c408
Aric Gardner <agardner@linuxfoundation.org>
2019-10-30 16:58:41 -0400

    Create gerrit project via gerrit api
    internal jenkins implementation:
    gerrit.linuxfoundation.org/infra/c/ci-management/+/62158
    
    Extend lfidapi to exit with good message and code if
    group does not exist. (check if group exists functionality)
    
    Remove pythonsix compatability for config parser
    
    extend config parser with has_option so we can use
    [gerrit] for now, and later when passwords are set by gerrit
    by gerrit.fqdn section.
    
    extend lftools gerrit to:
    Create a project
    Submit a .gitreview
    Grant read to github user on repo
    Create INFO.yaml patchset
    Create INFO job in "releng"/jjb/project/info-project.yaml
    
    basically, if a new info file is submitted to info-master repo
    git diff-tree --no-commit-id -r $rev --diff-filter=A --name-only --
    
    Then we check if a gerrit repo exists
    with checkproject
    
    if not, we create it using the info file to set the owner.
    then we can create the group with lfidapi
    and invite or add base on provided info file
    
    rinse and repeat for jira etc.
    
    Example script used to create a project
    lfid=agardner
    set -x
    set -o errexit
    lftools lfidapi create-group
    o-ran-sc-gerrit-ric-plt-resource-status-processor-committers
    lftools lfidapi user $lfid
    o-ran-sc-gerrit-ric-plt-resource-status-processor-committers
    lftools gerrit createproject gerrit.o-ran-sc.org/r/
    ric-plt/resource-status-processor
    o-ran-sc-gerrit-ric-plt-resource-status-processor-committers
    lftools gerrit prepareproject gerrit.o-ran-sc.org/r/
    ric-plt/resource-status-processor
    gerrit.o-ran-sc.org/ric-plt/resource-status-processor/INFO.yaml
    lftools gerrit addinfojob gerrit.o-ran-sc.org/r/
    ric-plt/resource-status-processor ci-management
    lftools github create-repo o-ran-sc ric-plt-resource-status-processor
    "Mirror of the ric-plt/resource-status-processor repo"
    lftools github update-repo o-ran-sc ric-plt-resource-status-processor
    --add_team Replication
    ssh -p 29418 $lfid@gerrit.o-ran-sc.org replication start --wait
    ric-plt/resource-status-processor
    lftools lfidapi match-ldap-to-info
    gerrit.o-ran-sc.org/ric-plt/resource-status-processor/INFO.yaml
    o-ran-sc-gerrit-ric-plt-resource-status-processor-committers
    
    Example output:
    https://gist.github.com/Aricg/715c15d6534598d2fe2519a075f001f7
    
    ISSUE: RELENG-2370
    Signed-off-by: Aric Gardner <agardner@linuxfoundation.org>
    Change-Id: I3a24751eeb2e739dee183074bbef6742df58a793

--- lftools/cli/gerrit.py
+++ lftools/cli/gerrit.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # SPDX-License-Identifier: EPL-1.0
 ##############################################################################
 # Copyright (c) 2018 The Linux Foundation and others.
@@ -11,10 +12,12 @@
 
 from __future__ import print_function
 
-import subprocess
-import sys
-
 import click
+
+from lftools.gerrit_helper import helper_addinfofile
+from lftools.gerrit_helper import helper_addinfojob
+from lftools.gerrit_helper import helper_createproject
+from lftools.gerrit_helper import helper_prepareproject
 
 
 @click.group()
@@ -24,39 +27,95 @@ def gerrit_cli(ctx):
     pass
 
 
-@click.command(name='create')
+@click.command(name='addinfofile')
 @click.argument('gerrit_url')
-@click.argument('ldap_group')
-@click.argument('repo')
-@click.argument('user')
-@click.option('--enable', is_flag=True,
-              help='Enable replication to Github.')
-@click.option('--parent', type=str, required=False,
-              help='Specify parent other than "All-Projects".')
+@click.argument('gerrit_project')
+@click.argument('info_file')
 @click.pass_context
-def create(
-        ctx, gerrit_url, ldap_group, repo, user, enable, parent):
-    """Create and configure permissions for a new gerrit repo.
+def addinfofile(ctx, gerrit_url, gerrit_project, info_file):
+    """Add an INFO file for review to a Project.
 
-    GERRIT_URL: server fqdn ex: gerrit.localhost
+    Requires gerrit directory.
 
-    LDAP_GROUP: owner ex: project-gerrit-group-committers
+    Example:
 
-    REPO: repo name ex: testrepo
-
-    USER: user that has permissions in gerrit
+    gerrit_url gerrit.o-ran-sc.org/r
+    gerrit_project test/test1
     """
-    params = ['gerrit_create']
-    params.extend(["-s", gerrit_url])
-    params.extend(["-o", ldap_group])
-    params.extend(["-r", repo])
-    params.extend(["-u", user])
-    if parent:
-        params.extend(["-p", parent])
-    if enable:
-        params.extend(["-e"])
-    status = subprocess.call(params)
-    sys.exit(status)
+    helper_addinfofile(ctx, gerrit_url, gerrit_project, info_file)
 
 
-gerrit_cli.add_command(create)
+@click.command(name='addinfojob')
+@click.argument('gerrit_url')
+@click.argument('gerrit_project')
+@click.argument('jjbrepo')
+@click.option('--reviewid', type=str, required=False,
+              help='ammend a review rather than making a new one')
+@click.pass_context
+def addinfojob(ctx, gerrit_url, gerrit_project, jjbrepo, reviewid):
+    """Add an INFO job for a new Project.
+
+    Adds info verify jenkins job for project.
+    result['id'] can be used to ammend a review
+    so that multiple projects can have info jobs added
+    in a single review
+
+    Example:
+
+    gerrit_url gerrit.o-ran-sc.org/r
+    gerrit_project test/test1
+    jjbrepo ci-mangement
+    """
+    helper_addinfojob(ctx, gerrit_url, gerrit_project, jjbrepo, reviewid)
+
+
+@click.command(name='prepareproject')
+@click.argument('gerrit_url')
+@click.argument('gerrit_project')
+@click.argument('info_file')
+@click.pass_context
+def prepareproject(ctx, gerrit_url, gerrit_project, info_file):
+    """Prepare a newly created project.
+
+    Newly created project is given a .gitreview.
+    Github group is given read
+    INFO.yaml is submitted for review.
+
+    Example:
+
+    gerrit_url gerrit.o-ran-sc.org/r
+    gerrit_project test/test1
+    TODO: prolog rule that dissalows self submit
+    """
+    helper_prepareproject(ctx, gerrit_url, gerrit_project, info_file)
+
+
+# Creates a gerrit project if project does not exist and adds ldap group as owner.
+# TODO: does not support inherited permissions from other than All-Projects.
+@click.command(name='createproject')
+@click.argument('gerrit_url')
+@click.argument('gerrit_project')
+@click.argument('ldap_group')
+@click.option('--check', is_flag=True,
+              help='just check if the project exists')
+@click.pass_context
+def createproject(ctx, gerrit_url, gerrit_project, ldap_group, check):
+    """Create a project via the gerrit API.
+
+    Creates a gerrit project.
+    Sets ldap group as owner.
+
+    Example:
+
+    gerrit_url gerrit.o-ran-sc.org/r
+    gerrit_project test/test1
+    ldap_group oran-gerrit-test-test1-committers
+
+    """
+    helper_createproject(ctx, gerrit_url, gerrit_project, ldap_group, check)
+
+
+gerrit_cli.add_command(addinfojob)
+gerrit_cli.add_command(addinfofile)
+gerrit_cli.add_command(prepareproject)
+gerrit_cli.add_command(createproject)
