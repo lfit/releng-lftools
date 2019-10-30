fcac412e3b4293faccc357ea86c4b150e01f8d28
Aric Gardner <agardner@linuxfoundation.org>
2019-10-30 16:58:41 -0400

    Create gerrit project via gerrit api
    internal jenkins implementation:
    gerrit.linuxfoundation.org/infra/c/ci-management/+/62158
    
    Extend lfidapi to exit with good message and code if
    group does not exist. (check if group exists functionality)
    
    Remove pythonsix compatability for config parser
    
    Usage: lftools gerrit [OPTIONS] COMMAND [ARGS]...
    
      GERRIT TOOLS.
    
    Options:
      --help  Show this message and exit.
    
    Commands:
      abandonchanges            Abandon all OPEN changes for a gerrit
    project.
      addfile                   Add an file for review to a Project.
      addgithubrights           Grant Github read for a project.
      addgitreview              Add git review to a project.
      addinfojob                Add an INFO job for a new Project.
      createproject             Create a project via the gerrit API.
      list-project-permissions  List Owners of a Project.
    
    ISSUE: RELENG-2370
    Signed-off-by: Aric Gardner <agardner@linuxfoundation.org>
    Change-Id: I3a24751eeb2e739dee183074bbef6742df58a793

--- lftools/cli/gerrit.py
+++ lftools/cli/gerrit.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # SPDX-License-Identifier: EPL-1.0
 ##############################################################################
 # Copyright (c) 2018 The Linux Foundation and others.
@@ -11,10 +12,14 @@
 
 from __future__ import print_function
 
-import subprocess
-import sys
+import logging
+from pprint import pformat
 
 import click
+
+from lftools.api.endpoints import gerrit
+
+log = logging.getLogger(__name__)
 
 
 @click.group()
@@ -24,39 +29,148 @@ def gerrit_cli(ctx):
     pass
 
 
-@click.command(name='create')
-@click.argument('gerrit_url')
-@click.argument('ldap_group')
-@click.argument('repo')
-@click.argument('user')
-@click.option('--enable', is_flag=True,
-              help='Enable replication to Github.')
-@click.option('--parent', type=str, required=False,
-              help='Specify parent other than "All-Projects".')
+@click.command(name='addfile')
+@click.argument('gerrit_fqdn')
+@click.argument('gerrit_project')
+@click.argument('filename')
+@click.option('--issue_id', type=str, required=False,
+              help='For projects that enforce an issue id for changesets')
+@click.option('--file_location', type=str, required=False,
+              help='option allos you to specify full path and file name')
 @click.pass_context
-def create(
-        ctx, gerrit_url, ldap_group, repo, user, enable, parent):
-    """Create and configure permissions for a new gerrit repo.
+def addfile(ctx, gerrit_fqdn, gerrit_project, filename, issue_id, file_location):
+    """Add an file for review to a Project.
 
-    GERRIT_URL: server fqdn ex: gerrit.localhost
+    Requires gerrit directory.
 
-    LDAP_GROUP: owner ex: project-gerrit-group-committers
+    Example:
 
-    REPO: repo name ex: testrepo
-
-    USER: user that has permissions in gerrit
+    gerrit_url gerrit.o-ran-sc.org/r
+    gerrit_project test/test1
     """
-    params = ['gerrit_create']
-    params.extend(["-s", gerrit_url])
-    params.extend(["-o", ldap_group])
-    params.extend(["-r", repo])
-    params.extend(["-u", user])
-    if parent:
-        params.extend(["-p", parent])
-    if enable:
-        params.extend(["-e"])
-    status = subprocess.call(params)
-    sys.exit(status)
+    g = gerrit.Gerrit(fqdn=gerrit_fqdn)
+    data = g.add_file(gerrit_fqdn, gerrit_project, filename, issue_id, file_location)
+    log.info(pformat(data))
 
 
-gerrit_cli.add_command(create)
+@click.command(name='addinfojob')
+@click.argument('gerrit_fqdn')
+@click.argument('gerrit_project')
+@click.argument('jjbrepo')
+@click.option('--reviewid', type=str, required=False,
+              help='ammend a review rather than making a new one')
+@click.option('--issue_id', type=str, required=False,
+              help='For projects that enforce an issue id for changesets')
+@click.pass_context
+def addinfojob(ctx, gerrit_fqdn, gerrit_project, jjbrepo, reviewid, issue_id):
+    """Add an INFO job for a new Project.
+
+    Adds info verify jenkins job for project.
+    result['id'] can be used to ammend a review
+    so that multiple projects can have info jobs added
+    in a single review
+
+    Example:
+
+    gerrit_url gerrit.o-ran-sc.org/r
+    gerrit_project test/test1
+    jjbrepo ci-mangement
+    """
+    g = gerrit.Gerrit(fqdn=gerrit_fqdn)
+    data = g.add_info_job(gerrit_fqdn, gerrit_project, jjbrepo, reviewid, issue_id)
+    log.info(pformat(data))
+
+
+@click.command(name='addgitreview')
+@click.argument('gerrit_fqdn')
+@click.argument('gerrit_project')
+@click.option('--issue_id', type=str, required=False,
+              help='For projects that enforce an issue id for changesets')
+@click.pass_context
+def addgitreview(ctx, gerrit_fqdn, gerrit_project, issue_id):
+    """Add git review to a project.
+
+    Example:
+    gerrit_url gerrit.o-ran-sc.org
+    gerrit_project test/test1
+    """
+    g = gerrit.Gerrit(fqdn=gerrit_fqdn)
+    data = g.add_git_review(gerrit_fqdn, gerrit_project, issue_id)
+    log.info(pformat(data))
+
+
+@click.command(name='addgithubrights')
+@click.argument('gerrit_fqdn')
+@click.argument('gerrit_project')
+@click.pass_context
+def addgithubrights(ctx, gerrit_fqdn, gerrit_project):
+    """Grant Github read for a project.
+
+    gerrit_url gerrit.o-ran-sc.org
+    gerrit_project test/test1
+    """
+    g = gerrit.Gerrit(fqdn=gerrit_fqdn)
+    data = g.add_github_rights(gerrit_fqdn, gerrit_project)
+    log.info(pformat(data))
+
+
+@click.command(name='abandonchanges')
+@click.argument('gerrit_fqdn')
+@click.argument('gerrit_project')
+@click.pass_context
+def abandonchanges(ctx, gerrit_fqdn, gerrit_project):
+    """Abandon all OPEN changes for a gerrit project.
+
+    gerrit_url gerrit.o-ran-sc.org
+    gerrit_project test/test1
+    """
+    g = gerrit.Gerrit(fqdn=gerrit_fqdn)
+    data = g.abandon_changes(gerrit_fqdn, gerrit_project)
+    log.info(pformat(data))
+
+# Creates a gerrit project if project does not exist and adds ldap group as owner.
+# TODO: does not support inherited permissions from other than All-Projects.
+@click.command(name='createproject')
+@click.argument('gerrit_fqdn')
+@click.argument('gerrit_project')
+@click.argument('ldap_group')
+@click.option('--check', is_flag=True,
+              help='just check if the project exists')
+@click.pass_context
+def createproject(ctx, gerrit_fqdn, gerrit_project, ldap_group, check):
+    """Create a project via the gerrit API.
+
+    Creates a gerrit project.
+    Sets ldap group as owner.
+
+    Example:
+
+    gerrit_url gerrit.o-ran-sc.org/r
+    gerrit_project test/test1
+    ldap_group oran-gerrit-test-test1-committers
+
+    """
+    g = gerrit.Gerrit(fqdn=gerrit_fqdn)
+    data = g.create_project(gerrit_fqdn, gerrit_project, ldap_group, check)
+    log.info(pformat(data))
+
+
+@click.command(name='list-project-permissions')
+@click.argument('gerrit_fqdn')
+@click.argument('project')
+@click.pass_context
+def list_project_permissions(ctx, gerrit_fqdn, project):
+    """List Owners of a Project."""
+    g = gerrit.Gerrit(fqdn=gerrit_fqdn)
+    data = g.list_project_permissions(project)
+    for ldap_group in data:
+        log.info(pformat(ldap_group))
+
+
+gerrit_cli.add_command(addinfojob)
+gerrit_cli.add_command(addfile)
+gerrit_cli.add_command(addgitreview)
+gerrit_cli.add_command(addgithubrights)
+gerrit_cli.add_command(createproject)
+gerrit_cli.add_command(abandonchanges)
+gerrit_cli.add_command(list_project_permissions)
