4c47cc96d5e00c432a97b3f4d7bbadd90a29c44d
Aric Gardner <agardner@linuxfoundation.org>
2019-10-30 16:58:41 -0400

    Create gerrit project via gerrit api
    Create a project,
    
    also submit a gitreview file.
    
    basically, if a new info file is submitted to info-master repo
    git diff-tree --no-commit-id -r $rev --diff-filter=A --name-only --
    
    Then we check if a gerrit repo exists
    with checkproject
    
    if not, we create it using the info file to set the owner.
    then we can create the group with lfidapi
    and invite or add,
    
    rinse and repeat for jira etc.
    
    Very WIP
    
    Signed-off-by: Aric Gardner <agardner@linuxfoundation.org>
    Change-Id: I3a24751eeb2e739dee183074bbef6742df58a793

--- lftools/cli/gerrit.py
+++ lftools/cli/gerrit.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # SPDX-License-Identifier: EPL-1.0
 ##############################################################################
 # Copyright (c) 2018 The Linux Foundation and others.
@@ -11,10 +12,18 @@
 
 from __future__ import print_function
 
+import requests
 import subprocess
 import sys
+import json
+import re
+import urllib
+#from urllib.parse import urlparse
 
 import click
+from pygerrit2 import GerritRestAPI, HTTPBasicAuth
+from requests.utils import get_netrc_auth
+from lftools import config
 
 
 @click.group()
@@ -23,6 +32,289 @@ def gerrit_cli(ctx):
     """GERRIT TOOLS."""
     pass
 
+
+# Helper to list all projects.
+def listprojects(gerrit_url):
+    user = config.get_setting("gerrit", "username")
+    pass1 = config.get_setting("gerrit", "password")
+    auth = HTTPBasicAuth(user, pass1)
+    url = ("https://{}".format(gerrit_url))
+    rest = GerritRestAPI(url=url, auth=auth)
+    headers = {'Content-Type': 'application/json; charset=UTF-8'}
+    access_str = '/projects/'
+    result = rest.get(access_str, headers=headers)
+
+    all_projects = []
+    for name, values in result.items():
+        idlink = (values['id'])
+        state = (values['state'])
+        if state == "ACTIVE":
+            all_projects.append(idlink)
+    return(all_projects)
+
+
+
+#This actually creates a change and then post a gitreview
+@click.command(name='checkproject')
+@click.argument('gerrit_url')
+@click.argument('gerrit_project')
+@click.pass_context
+def checkproject(ctx, gerrit_url, gerrit_project):
+    user = config.get_setting("gerrit", "username")
+    pass1 = config.get_setting("gerrit", "password")
+    auth = HTTPBasicAuth(user, pass1)
+    url = ("https://{}".format(gerrit_url))
+    rest = GerritRestAPI(url=url, auth=auth)
+    headers = {'Content-Type': 'application/json; charset=UTF-8'}
+    gerrit_project_encoded = urllib.parse.quote(gerrit_project, safe='', encoding=None, errors=None)
+    list = [ 'projects/', 'projects/{}'.format(gerrit_project_encoded) ]
+    #check if gerrit exists, check if project exists
+    for access_str in list:
+        try:
+            result = rest.get(access_str, headers=headers)
+        except:
+            print("Not found {}{}".format(url, access_str))
+            sys.exit(1)
+        print("found {}{}".format(url, access_str))
+    access_str = 'projects/{}'.format(gerrit_project_encoded)
+
+#    access_str = '/changes/68739'
+#    result = rest.get(access_str, headers=headers)
+#    print(result)
+
+
+    payload = json.dumps({
+        "project" : '{}'.format(gerrit_project),
+        "subject" : 'Automation adds Gitreview \n\nSigned-off-by: Aric Gardner <agardner@linuxfoundation.org>',
+        "branch" : 'master',
+        })
+
+    access_str = 'changes/'
+    print(payload)
+    result = rest.post(access_str, headers=headers, data=payload)
+    print(result)
+
+    print(result['id'])
+    changeid = (result['id'])
+    changesmall = (result['change_id'])
+    
+    my_inline_file = """
+    [gerrit]
+    host=gerrit.linuxfoundation.org
+    port=29418
+    project=releng/lftools.git
+    defaultbranch=master
+    """
+    my_inline_file_size = len(my_inline_file.encode('utf-8'))
+    print(my_inline_file_size)
+
+    headers = {'Content-Type': 'text/plain',
+               'Content-length': '55'}
+
+    access_str = 'changes/{}/edit/.gitreview'.format(changeid)
+    payload = my_inline_file
+    result = rest.put(access_str, headers=headers, data=payload)
+    print(result)
+
+
+    access_str = 'changes/{}/edit:publish'.format(changeid)
+    headers = {'Content-Type': 'application/json; charset=UTF-8'}
+    payload = json.dumps({
+        "notify" : "NONE",
+        })
+    result = rest.post(access_str, headers=headers, data=payload)
+
+    #result = rest.post(access_str, headers=headers)
+    print(result)
+
+#    'POST /changes/{change-id}/edit:publish'
+
+
+
+@click.command(name='createproject')
+@click.argument('gerrit_url')
+@click.argument('gerrit_project')
+@click.argument('ldap_group')
+@click.pass_context
+def createproject(ctx, gerrit_url, gerrit_project, ldap_group):
+    user = config.get_setting("gerrit", "username")
+    pass1 = config.get_setting("gerrit", "password")
+    auth = HTTPBasicAuth(user, pass1)
+    url = ("https://{}".format(gerrit_url))
+    rest = GerritRestAPI(url=url, auth=auth)
+    headers = {'Content-Type': 'application/json; charset=UTF-8'}
+    gerrit_project = urllib.parse.quote(gerrit_project, safe='', encoding=None, errors=None)
+
+    access_str = 'projects/{}'.format(gerrit_project)
+    try:
+        result = rest.get(access_str, headers=headers)
+        print("found {}{}".format(url, access_str))
+        projectexists = True
+    except:
+        projectexists = False
+        print("not found {}{}".format(url, access_str))
+        
+    if projectexists:
+        print("Project already exists")
+
+    else:
+        ldapgroup = "ldap:cn={},ou=Groups,dc=freestandards,dc=org".format(ldap_group)
+
+        access_str = 'projects/{}'.format(gerrit_project)
+        payload = json.dumps({
+        "description": "This is a demo project.",
+        "submit_type": "INHERIT",
+        "create_empty_commit" : "True",
+        "owners": [
+          "{}".format(ldapgroup)
+        ]
+        })
+        print(payload)
+
+        result = rest.put(access_str, headers=headers, data=payload)
+        print(result)
+        
+    
+@click.command(name='fixaccessrights')
+@click.option('--projectid', type=str, required=False,
+              help='Check only one project')
+@click.argument('gerrit_url')
+@click.option('--githubid', type=str, required=False,
+              help='daebbf523be390041a1a9c0e340a6d833aae5043')
+@click.option('--dryrun', is_flag=True,
+              help='show post, but dont post')
+@click.pass_context
+def fixaccessrights(ctx, projectid, gerrit_url, githubid, dryrun):
+    user = config.get_setting("gerrit", "username")
+    pass1 = config.get_setting("gerrit", "password")
+    auth = HTTPBasicAuth(user, pass1)
+    url = ("https://{}/r".format(gerrit_url))
+    rest = GerritRestAPI(url=url, auth=auth)
+    headers = {'Content-Type': 'application/json; charset=UTF-8'}
+    all_projects = []
+
+    #If a projectid is passed dont generate a list of projects from list projects
+    if projectid:
+        all_projects.append(projectid)
+    else: 
+        all_projects = listprojects(gerrit_url=gerrit_url)
+
+    for projectid in all_projects:
+        #if projectid == "All-Projects" or projectid == "All-Users":
+        #    print("--- Skipping {} ---".format(projectid))
+        if not re.search("%2F", projectid):
+            print("--- Skipping Top level project {} ---".format(projectid))
+        else:
+            access_str = 'projects/{}/access'.format(projectid)
+            result = rest.get(access_str, headers=headers)
+            print("Project:", projectid)
+
+            if 'inherits_from' in result:
+                inherits = (result['inherits_from']['id'])
+                print("    Inherits from:", inherits)
+                if inherits != "All-Projects":
+                    output = accessrights(projectid=projectid,
+                                          gerrit_url=gerrit_url,
+                                          setparent=True,
+                                          setowner=False,
+                                          githubid=False,
+                                          dryrun=dryrun)
+
+                    print(output)
+            else:
+                print("no key")
+
+            try:
+                owner = (result['local']['refs/*']['permissions']['owner']['rules'])
+            except KeyError:
+                owner = "None"
+
+            if owner == "None":
+                print("    Owner:", owner)
+                output = accessrights(projectid=projectid,
+                                      gerrit_url=gerrit_url,
+                                      setparent=False,
+                                      setowner=True,
+                                      githubid=False,
+                                      dryrun=dryrun)
+                print(output)
+
+            else:
+                for name in owner.keys():
+                    print("    Owner:", name)
+
+
+            try:
+                github = (result['local']['refs/*']['permissions']['read']['rules'])
+            except KeyError:
+                github = "None"
+
+            if github == "None" and githubid:
+                print("    Github:", github)
+                output = accessrights(projectid=projectid,
+                                      gerrit_url=gerrit_url,
+                                      setparent=False,
+                                      setowner=False,
+                                      githubid=githubid,
+                                      dryrun=dryrun)
+                print(output)
+            else:
+                for name in github.keys():
+                    print("    Github:", name)
+
+def accessrights(projectid, gerrit_url, setparent, setowner, githubid, dryrun):
+    user = config.get_setting("gerrit", "username")
+    pass1 = config.get_setting("gerrit", "password")
+    auth = HTTPBasicAuth(user, pass1)
+    url = ("https://{}/r".format(gerrit_url))
+    rest = GerritRestAPI(url=url, auth=auth)
+
+    #print("This is Project ID:", projectid)
+    project_dashed = projectid.replace("%2F", "-")
+    headers = {'Content-Type': 'application/json; charset=UTF-8'}
+
+    if githubid:
+        payload = json.dumps({
+            "add": {
+                "refs/*": {
+                    "permissions": {
+                        "read": {
+                            "rules": {
+                                "{}".format(githubid) : {
+                                    "action": "{}".format("ALLOW")
+        }}}}}}
+        })
+
+    if setparent:
+        payload = json.dumps({
+            "parent": "All-Projects"
+        })
+
+    if setowner:
+        payload = json.dumps({
+            "add": {
+                "refs/*": {
+                    "permissions": {
+                        "owner": {
+                            "rules": {
+                                "ldap:cn=onap-gerrit-{}-committers,ou=Groups,dc=freestandards,dc=org".format(project_dashed): {
+                                    "action": "ALLOW",
+                                    "force": "false"
+            }}}}}}
+        })
+
+
+
+    access_str = 'projects/{}/access'.format(projectid)
+
+    if dryrun:
+        return("        -Dry Run not modifying {}".format(payload))
+    else:
+        print("not doing this")
+
+        result = rest.post(access_str, headers=headers, data=payload)
+        pretty = json.dumps(result, indent=4, sort_keys=True)
+        return(pretty)
 
 @click.command(name='create')
 @click.argument('gerrit_url')
@@ -60,3 +352,6 @@ def create(
 
 
 gerrit_cli.add_command(create)
+gerrit_cli.add_command(fixaccessrights)
+gerrit_cli.add_command(checkproject)
+gerrit_cli.add_command(createproject)
