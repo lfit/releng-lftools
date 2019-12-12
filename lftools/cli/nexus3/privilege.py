f1035d70850567f771c901ce35e235f5524d53c2
DW Talton <dtalton@contractor.linuxfoundation.org>
2019-12-11 17:26:02 -0700

    Implement the Nexus 3 REST API in lftools
    Issue: RELENG-308
    Signed-off-by: DW Talton <dtalton@contractor.linuxfoundation.org>
    Change-Id: I3dc1b1b7280fe6a2d69a75e2f8c7f5a9d64c5126

---
+++ lftools/cli/nexus3/privilege.py
@@ -0,0 +1,22 @@
+import click
+import logging
+from tabulate import tabulate
+from pprint import pformat
+from lftools.api.endpoints import nexus
+
+
+log = logging.getLogger(__name__)
+
+
+@click.group()
+@click.pass_context
+def privilege(ctx):
+    pass
+
+
+@privilege.command(name='list')
+@click.pass_context
+def list_privileges(ctx):
+    r = ctx.obj['nexus']
+    data = r.list_privileges()
+    log.info(tabulate(data, headers=["Type", "Name", "Description", "Read Only"]))
