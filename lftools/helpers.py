ab546077f8b7282efe7fae3a28bf00a01904f1a1
DW Talton <dtalton@contractor.linuxfoundation.org>
2020-03-29 14:15:01 -0700

    Add utils command
    Add utils command to hold utilities like password generators
    and whatnot.
    
    Issue-ID: RELENG-2754
    Signed-off-by: DW Talton <dtalton@contractor.linuxfoundation.org>
    Change-Id: I968c73f52a87c4436c355a1b9b66a49128a3bf56

---
+++ lftools/helpers.py
@@ -0,0 +1,8 @@
+import random
+import string
+
+
+def generate_password(length=12):
+    punctuation = "!#$%&()*+,-.:;<=>?@[]^_{|}~"
+    password_characters = string.ascii_letters + string.digits + punctuation
+    return "".join(random.choice(password_characters) for _ in range(length))
