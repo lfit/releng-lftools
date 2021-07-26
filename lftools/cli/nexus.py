194a57f106094b6e044ead68a0a63af5bc7bfa25
Bengt Thuree <bthuree@linuxfoundation.org>
2021-07-26 15:47:22 +1000

    Fix: Force delay between runs due to throttling
    Using filename when checking / storing last_run timestamp
    Storing Tuples (IP - Time), so multiple servers can be used.
    
    Checking for anonymous throttling stats
    
    Storing files in an permanent S3 bucket
    
    Signed-off-by: Bengt Thuree <bthuree@linuxfoundation.org>
    Change-Id: I778f012e97d7b2663d0eeb68ec778f481310bc89

--- lftools/cli/nexus.py
+++ lftools/cli/nexus.py
@@ -226,11 +226,42 @@ def release(ctx, repos, verify, server):
     " File sample:                                                  "
     r" ^\d+.\d+.\d+$                                                 ",
 )
+@click.option(
+    "-i",
+    "--ignorethrottledelay",
+    is_flag=True,
+    required=False,
+    default=False,
+    help="Ignore checking for throttle delay.",
+)
+@click.option(
+    "-3",
+    "--s3_bucket_name",
+    type=str,
+    default="",
+    required=False,
+    help="Which S3 bucket to use for permanent data storage.",
+)
 @click.pass_context
-def copy_from_nexus3_to_dockerhub(ctx, org, repo, exact, summary, verbose, copy, progbar, repofile, version_regexp):
+def copy_from_nexus3_to_dockerhub(
+    ctx,
+    org,
+    repo,
+    exact,
+    summary,
+    verbose,
+    copy,
+    progbar,
+    repofile,
+    version_regexp,
+    ignorethrottledelay,
+    s3_bucket_name,
+):
     """Find missing repos in Docker Hub, Copy from Nexus3.
 
     Will by default list all missing repos in Docker Hub, compared to Nexus3.
     If -c (--copy) is provided, it will copy the repos from Nexus3 to Docker Hub.
     """
-    rdh.start_point(org, repo, exact, summary, verbose, copy, progbar, repofile, version_regexp)
+    rdh.start_point(
+        org, repo, exact, summary, verbose, copy, progbar, repofile, version_regexp, ignorethrottledelay, s3_bucket_name
+    )
