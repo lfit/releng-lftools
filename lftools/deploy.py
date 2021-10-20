d36fd3d90b0a90ba11db4c97547d647425c036e0
Anil Belur <abelur@linuxfoundation.org>
2021-10-20 13:32:44 +1000

    Fix: Create temp file in empty dirs for S3 log dir
    Since S3 buckets does not have an underlying file system to view empty
    directories, an temp file has to be created when referencing the directory.
    This allows empty folders to be viewed through cloudfront, without
    returning a 404 error.
    
    Create temp file in the format: "_%d%m%Y_%H%M%S", would
    help with future clean or debugging.
    
    Issue-ID: RELENG-4016
    Change-Id: I0a635cd32da860deaada0ac0cc15bf1aa5fc2d4a
    Signed-off-by: Anil Belur <abelur@linuxfoundation.org>

--- lftools/deploy.py
+++ lftools/deploy.py
@@ -11,6 +11,7 @@
 """Library of functions for deploying artifacts to Nexus."""
 
 import concurrent.futures
+import datetime
 import errno
 import glob
 import gzip
@@ -268,7 +269,6 @@ def copy_archives(workspace, pattern=None):
     log.debug("Files found: {}".format(paths))
 
     no_dups_paths = _remove_duplicates_and_sort(paths)
-
     for src in no_dups_paths:
         if len(os.path.basename(src)) > 255:
             log.warn("Filename {} is over 255 characters. Skipping...".format(os.path.basename(src)))
@@ -286,6 +286,14 @@ def copy_archives(workspace, pattern=None):
         else:
             log.info("Not copying directories: {}.".format(src))
 
+    # Create a temp file to handle empty dirs in AWS S3 buckets.
+    now = datetime.datetime.now()
+    p = now.strftime("_%d%m%Y_%H%M%S")
+    for dirpath, dirnames, files in os.walk(dest_dir):
+        if not files:
+            fd, tmp = tempfile.mkstemp(prefix=p, dir=dirpath)
+            os.close(fd)
+            log.debug("temp file created in dir: {}.".format(dirpath))
 
 def deploy_archives(nexus_url, nexus_path, workspace, pattern=None):
     """Archive files to a Nexus site repository named logs.
