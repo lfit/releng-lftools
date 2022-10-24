36c43d3d0d5123cee0e6e140cc0bd1a634827c65
Bengt Thuree <bthuree@linuxfoundation.org>
2022-10-24 15:54:47 +1100

    Chore: Adding unit test for ReleaseStaging
    To capture NotAllowedUpdate error
    
    Signed-off-by: Bengt Thuree <bthuree@linuxfoundation.org>
    Change-Id: I5c62071f49c9278af6287d38f2896837ff0a821b

---
+++ tests/test_release_staging_repo.py
@@ -0,0 +1,56 @@
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2018 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+"""Test deploy command."""
+
+import os
+
+import pytest
+import requests
+import responses
+
+import lftools.nexus.cmd as test_cmd
+from lftools.nexus import Nexus, util
+
+
+FIXTURE_DIR = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)),
+    "fixtures",
+)
+
+@pytest.mark.datafiles(
+    os.path.join(FIXTURE_DIR, "nexus"),
+)
+class TestReleaseStagingRepos():
+
+    def mocked_get_credentials(self, nothing, url):
+        """Mocking Get Credentials."""
+        return {"nexus": url, "user": "nexus", "password": "nexus"}
+
+    def test_releasestagin_notallowedupdate(self, datafiles, responses, mocker):
+        nexus_url = "http://my.gerrit.org/"
+        nexus_url_check = "http://my.gerrit.org/service/local/repo_targets"
+        mocker.patch("lftools.nexus.cmd.get_credentials", side_effect=self.mocked_get_credentials)
+        repo = "autorelease-348900"
+        mock_url = "{}service/local/staging/repository/{}/activity".format(nexus_url, repo)
+     
+        release_staging_xml_result = os.path.join(str(datafiles), "ReleaseStaging-NotAllowUpdatingArtifact.xml")
+    
+        with open (release_staging_xml_result, 'r') as file:
+          nexus_answer = file.read()
+
+        responses.add(responses.GET, mock_url, body=nexus_answer, status=200)
+        responses.add(responses.GET, nexus_url_check, body=nexus_answer, status=200)
+
+        with pytest.raises(SystemExit) as cm:
+            test_cmd.release_staging_repos([repo], True, nexus_url)
+
+        assert cm.type == SystemExit
+        assert cm.value.code == 1
+
